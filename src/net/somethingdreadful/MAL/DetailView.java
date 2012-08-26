739a701820f4f1b178454f386a065fd91b9d292c
Chris Fanti <animabolt@gmail.com>
2012-08-26 17:52:16 -0400

    Removed unused imports

--- src/net/somethingdreadful/MAL/DetailView.java
+++ src/net/somethingdreadful/MAL/DetailView.java
@@ -3,30 +3,16 @@ package net.somethingdreadful.MAL;
 import net.somethingdreadful.MAL.R;
 import android.app.ActionBar;
 import android.app.DialogFragment;
-import android.app.FragmentTransaction;
 import android.content.Context;
 import android.graphics.drawable.BitmapDrawable;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.app.NavUtils;
-import android.support.v4.view.ViewPager;
-import android.view.Gravity;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.view.ViewTreeObserver;
-import android.view.ViewTreeObserver.OnGlobalLayoutListener;
-import android.widget.FrameLayout;
-import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
-import android.widget.LinearLayout.LayoutParams;
 
 public class DetailView extends FragmentActivity implements DetailsBasicFragment.IDetailsBasicAnimeFragment {
 
