f5f2dfe2fcfee7c2b7d10cda2fa08f119601aede
Michael Johnson <youngmug@animeneko.net>
2012-09-18 12:26:20 -0700

    Cleanup field input
    Commit 15a7969a introduced a fix to a crash error where whitespace at the
    end of a username could cause a crash bug. This commit removes the
    replace and instead uses trim on the string. It also does the same for
    the password field as the user may make a similar error there as well.

--- src/net/somethingdreadful/MAL/FirstTimeInit.java
+++ src/net/somethingdreadful/MAL/FirstTimeInit.java
@@ -1,6 +1,5 @@
 package net.somethingdreadful.MAL;
 
-import net.somethingdreadful.MAL.R;
 import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Context;
@@ -17,112 +16,113 @@ import android.widget.Toast;
 
 
 public class FirstTimeInit extends Activity {
-	static EditText malUser;
-	static EditText malPass;
-	static String testMalUser;
-	static String testMalPass;
-	static Button connectButton;
-	static ProgressDialog pd;
-	static Thread netThread;
-	static Context context;
-	static private Handler messenger;
-	static PrefManager prefManager;
-	
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
+    static EditText malUser;
+    static EditText malPass;
+    static String testMalUser;
+    static String testMalPass;
+    static Button connectButton;
+    static ProgressDialog pd;
+    static Thread netThread;
+    static Context context;
+    static private Handler messenger;
+    static PrefManager prefManager;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.firstrun);
-        
+
         malUser = (EditText) findViewById(R.id.edittext_malUser);
         malPass = (EditText) findViewById(R.id.edittext_malPass);
         Button connectButton = (Button) findViewById(R.id.button_connectToMal);
         Button registerButton = (Button) findViewById(R.id.registerButton);
         context = getApplicationContext();
-        
+
         prefManager = new PrefManager(context);
-        
+
         connectButton.setOnClickListener(new OnClickListener()
         {
-        	public void onClick(View v)
-        	{
-        		testMalUser = malUser.getText().toString().replace(" ", "");
-        		testMalPass = malPass.getText().toString();
-        		tryConnection();
-        	}
-        	
+            public void onClick(View v)
+            {
+                testMalUser = malUser.getText().toString().trim();
+                testMalPass = malPass.getText().toString().trim();
+                tryConnection();
+            }
+
         });
-        
+
         registerButton.setOnClickListener(new OnClickListener()
         {
-			public void onClick(View arg0) {
-				Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://myanimelist.net/register.php"));
-				startActivity(browserIntent);
-				
-			}
+            public void onClick(View arg0) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://myanimelist.net/register.php"));
+                startActivity(browserIntent);
+
+            }
         });
-        
+
         messenger = new Handler() {
-            public void handleMessage(Message msg) 
+            @Override
+            public void handleMessage(Message msg)
             {
-            	if (msg.what == 2)
-            	{ 
-            		pd.dismiss();
-            		
-            		Toast.makeText(context, context.getString(R.string.toast_VerifyProblem), Toast.LENGTH_SHORT).show();
-            	}
-            	if (msg.what == 3)
-            	{
-            		pd.dismiss();
-            		
-            		Toast.makeText(context, context.getString(R.string.toast_AccountOK), Toast.LENGTH_SHORT).show();
-            		
-            		prefManager.setUser(testMalUser);
-    				prefManager.setPass(testMalPass);
-    				prefManager.setInit(true);
-    				prefManager.commitChanges();
-            		
-            		Intent goHome = new Intent(context, Home.class);
-                	goHome.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-                	.putExtra("net.somethingdreadful.MAL.firstSync", true);
-                	startActivity(goHome);
-            	}
-            	
-            super.handleMessage(msg);
+                if (msg.what == 2)
+                {
+                    pd.dismiss();
+
+                    Toast.makeText(context, context.getString(R.string.toast_VerifyProblem), Toast.LENGTH_SHORT).show();
+                }
+                if (msg.what == 3)
+                {
+                    pd.dismiss();
+
+                    Toast.makeText(context, context.getString(R.string.toast_AccountOK), Toast.LENGTH_SHORT).show();
+
+                    prefManager.setUser(testMalUser);
+                    prefManager.setPass(testMalPass);
+                    prefManager.setInit(true);
+                    prefManager.commitChanges();
+
+                    Intent goHome = new Intent(context, Home.class);
+                    goHome.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                    .putExtra("net.somethingdreadful.MAL.firstSync", true);
+                    startActivity(goHome);
+                }
+
+                super.handleMessage(msg);
             }
         };
     }
-	
-	
-	private void tryConnection()
-	{
-		pd = ProgressDialog.show(this, context.getString(R.string.dialog_Verifying), context.getString(R.string.dialog_VerifyingBlurb));
-		netThread = new networkThread();
-		netThread.start();
-		
-	}
-	
-	public class networkThread extends Thread
-	{
-		@Override
-		public void run()
-		{
-			boolean valid = MALManager.verifyAccount(testMalUser, testMalPass);
-			
-			String words = "";
-			Message msg = new Message();
-			
-			if (valid == false)
-			{
-				msg.what = 2;
-				messenger.sendMessage(msg);
-			}
-			else
-			{	
-				msg.what = 3;
-				messenger.sendMessage(msg);
-			}
-		}
-	}
-	
+
+
+    private void tryConnection()
+    {
+        pd = ProgressDialog.show(this, context.getString(R.string.dialog_Verifying), context.getString(R.string.dialog_VerifyingBlurb));
+        netThread = new networkThread();
+        netThread.start();
+
+    }
+
+    public class networkThread extends Thread
+    {
+        @Override
+        public void run()
+        {
+            boolean valid = MALManager.verifyAccount(testMalUser, testMalPass);
+
+            String words = "";
+            Message msg = new Message();
+
+            if (valid == false)
+            {
+                msg.what = 2;
+                messenger.sendMessage(msg);
+            }
+            else
+            {
+                msg.what = 3;
+                messenger.sendMessage(msg);
+            }
+        }
+    }
+
 
 }
