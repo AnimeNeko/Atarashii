2e90fe2d5316dbe973a89016b2b1f6382cb213f8
Chris Fanti <animabolt@gmail.com>
2012-10-30 01:26:26 -0400

    Settings work on 2.2
    Changed the Settings to use SherlockPreferenceActivity since
    PreferenceFragment isn't actually part of the compatibility library.
    Might revisit this later for a more elegant solution.

--- src/net/somethingdreadful/MAL/Settings.java
+++ src/net/somethingdreadful/MAL/Settings.java
@@ -1,44 +1,49 @@
 package net.somethingdreadful.MAL;
 
-import net.somethingdreadful.MAL.R;
-import android.app.ActionBar;
-import android.app.Activity;
 import android.os.Bundle;
-import android.preference.PreferenceActivity;
 import android.preference.PreferenceManager;
-import android.view.MenuItem;
 
-public class Settings extends Activity {
-	protected void onCreate(Bundle savedInstanceState)
-	{
-		super.onCreate(savedInstanceState);
-		
-		setTitle(R.string.title_activity_settings);
+import com.actionbarsherlock.app.ActionBar;
+import com.actionbarsherlock.app.SherlockPreferenceActivity;
+import com.actionbarsherlock.view.MenuItem;
 
-//		PreferenceManager prefMgr = getPreferenceManager();
-//		prefMgr.setSharedPreferencesName("prefs");
+public class Settings extends SherlockPreferenceActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
 
-		getFragmentManager().beginTransaction()
-        .replace(android.R.id.content, new SettingsFragment())
-        .commit();
+        setTitle(R.string.title_activity_settings);
 
-		ActionBar bar = getActionBar();
-		bar.setDisplayHomeAsUpEnabled(true);
+        //        //		PreferenceManager prefMgr = getPreferenceManager();
+        //        //		prefMgr.setSharedPreferencesName("prefs");
+        //
+        //        getSupportFragmentManager().beginTransaction()
+        //        .add(android.R.id.content, new SettingsFragment())
+        //        .commit();
 
-	}
-	
-	
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item)
-	{
-		switch(item.getItemId())
-		{
-		case android.R.id.home:
-			finish();
-		}
-		
-		
-		return true;
-	}
-	
+        ActionBar bar = getSupportActionBar();
+        bar.setDisplayHomeAsUpEnabled(true);
+
+        PreferenceManager prefMgr = getPreferenceManager();
+        prefMgr.setSharedPreferencesName("prefs");
+
+        addPreferencesFromResource(R.xml.settings);
+    }
+
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        switch(item.getItemId())
+        {
+            case android.R.id.home:
+                finish();
+        }
+
+
+        return true;
+    }
+
 }
--- src/net/somethingdreadful/MAL/SettingsFragment.java
+++ src/net/somethingdreadful/MAL/SettingsFragment.java
@@ -1,19 +1,16 @@
 package net.somethingdreadful.MAL;
 
-import android.os.Bundle;
-import android.preference.PreferenceFragment;
-import android.preference.PreferenceManager;
 
-public class SettingsFragment extends PreferenceFragment {
-
-		@Override
-		public void onCreate(Bundle state)
-		{
-			super.onCreate(state);
-			
-			PreferenceManager prefMgr = getPreferenceManager();
-			prefMgr.setSharedPreferencesName("prefs");
-			
-			addPreferencesFromResource(R.xml.settings);
-		}
-}
+//public class SettingsFragment extends PreferenceFragment {
+//
+//    @Override
+//    public void onCreate(Bundle state)
+//    {
+//        super.onCreate(state);
+//
+//        PreferenceManager prefMgr = getPreferenceManager();
+//        prefMgr.setSharedPreferencesName("prefs");
+//
+//        addPreferencesFromResource(R.xml.settings);
+//    }
+//}
