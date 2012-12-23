876885ef6a00106a2905e8474755343ab86cc90c
Chris Fanti <animabolt@gmail.com>
2012-12-23 16:47:26 -0500

    Dummy implementation of UpdateReceiver
    Added a dummy implementation of UpdateReceiver to test sending a
    broadcast from another app.

---
+++ src/net/somethingdreadful/MAL/UpdateReceiver.java
@@ -0,0 +1,16 @@
+package net.somethingdreadful.MAL;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class UpdateReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent receivedIntent) {
+        Toast.makeText(context, "Created by UpdateReceiver!", Toast.LENGTH_SHORT).show();
+
+    }
+
+}
