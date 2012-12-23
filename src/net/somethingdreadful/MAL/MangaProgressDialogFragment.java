66a6d88f2fadcfcb65920b81d3022bff64f08658
Chris Fanti <animabolt@gmail.com>
2012-12-23 14:40:00 -0500

    Proper Cancel/Update Buttons (2/2)
    This commit completes the previous commit, adding the Cancel/Update
    buttons to the manga progress picker thing.

--- src/net/somethingdreadful/MAL/MangaProgressDialogFragment.java
+++ src/net/somethingdreadful/MAL/MangaProgressDialogFragment.java
@@ -1,15 +1,20 @@
 package net.somethingdreadful.MAL;
 
 import net.simonvt.widget.NumberPicker;
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 
 public class MangaProgressDialogFragment extends DialogFragment {
+
+    View view;
 
     NumberPicker chapterPicker;
     NumberPicker volumePicker;
@@ -33,9 +38,36 @@ public class MangaProgressDialogFragment extends DialogFragment {
     }
 
     @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        Log.v("MALX", "onCreateDialog Fired");
+
+        view = getActivity().getLayoutInflater().inflate(R.layout.dialog_manga_progress, null);
+
+        return new AlertDialog.Builder(getActivity())
+        .setPositiveButton("Update", new DialogInterface.OnClickListener()
+        {
+            @Override
+            public void onClick(DialogInterface dialog, int whichButton)
+            {
+                ((DetailView) getActivity()).onMangaDialogDismissed(chapterPicker.getValue(), volumePicker.getValue());
+                dismiss();
+            }
+        }
+                ).setNegativeButton("Cancel", new DialogInterface.OnClickListener()
+                {
+                    @Override
+                    public void onClick(DialogInterface dialog, int whichButton)
+                    {
+                        dismiss();
+                    }
+                }
+                        ).setView(view).setTitle("I've read:").create();
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle state)
     {
-        View view = inflater.inflate(R.layout.dialog_manga_progress, container);
+        //        View view = inflater.inflate(R.layout.dialog_manga_progress, container);
 
         if (state == null)
         {
@@ -71,7 +103,7 @@ public class MangaProgressDialogFragment extends DialogFragment {
         chapterPicker = (NumberPicker) view.findViewById(R.id.chapterPicker);
         volumePicker = (NumberPicker) view.findViewById(R.id.volumePicker);
 
-        getDialog().setTitle("I've read:");
+        //        getDialog().setTitle("I've read:");
 
         chapterPicker.setMinValue(0);
         volumePicker.setMinValue(0);
@@ -100,7 +132,7 @@ public class MangaProgressDialogFragment extends DialogFragment {
         chapterPicker.setValue(chapterPickerValue);
         volumePicker.setValue(volumePickerValue);
 
-        return view;
+        return null;
     }
 
     @Override
@@ -114,7 +146,6 @@ public class MangaProgressDialogFragment extends DialogFragment {
     @Override
     public void onCancel(DialogInterface dialog)
     {
-        ((DetailView) getActivity()).onMangaDialogDismissed(chapterPicker.getValue(), volumePicker.getValue());
         this.dismiss();
     }
 
