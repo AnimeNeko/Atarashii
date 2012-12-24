7e6b697239618c3e0bfd0eea6db85655008d6f11
Michael Johnson <youngmug@animeneko.net>
2012-12-24 12:53:39 -0800

    About Activity Cleanup and Linking
    Cleaned up the horizontal layout for the about display. Also linked up
    contributor usernames to MAL profiles and linked to Unofficial API
    website.

--- src/net/somethingdreadful/MAL/About.java
+++ src/net/somethingdreadful/MAL/About.java
@@ -1,6 +1,8 @@
 package net.somethingdreadful.MAL;
 
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
+import android.widget.TextView;
 
 import com.actionbarsherlock.app.ActionBar;
 import com.actionbarsherlock.app.SherlockActivity;
@@ -9,21 +11,27 @@ import com.actionbarsherlock.view.MenuItem;
 public class About extends SherlockActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
 
         setTitle(R.string.title_activity_about);
         ActionBar bar = getSupportActionBar();
         bar.setDisplayHomeAsUpEnabled(true);
+
+        TextView animaMalContent = (TextView)findViewById(R.id.contributor_anima_name);
+        TextView motokochanMalContent = (TextView)findViewById(R.id.contributor_motokochan_name);
+        TextView acknowledgementsContent = (TextView) findViewById(R.id.acknowledgements_card_content);
+
+        animaMalContent.setMovementMethod(LinkMovementMethod.getInstance());
+        motokochanMalContent.setMovementMethod(LinkMovementMethod.getInstance());
+        acknowledgementsContent.setMovementMethod(LinkMovementMethod.getInstance());
+
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item)
-    {
-        switch(item.getItemId())
-        {
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
             case android.R.id.home:
                 finish();
         }
