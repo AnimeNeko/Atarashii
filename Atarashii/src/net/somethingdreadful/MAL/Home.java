1818ba0571c27be8d0e0edd97bdff522061f0bf9
Eric Yu <cyrieu@gmail.com>
2013-08-09 15:40:50 -0500

    Added a Nav Drawer Interface

--- Atarashii/src/net/somethingdreadful/MAL/Home.java
+++ Atarashii/src/net/somethingdreadful/MAL/Home.java
@@ -17,12 +17,21 @@ import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.app.NotificationCompat;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.actionbarsherlock.app.ActionBar;
 import com.actionbarsherlock.app.SherlockDialogFragment;
+import com.actionbarsherlock.app.SherlockFragmentActivity;
 import com.actionbarsherlock.view.Menu;
 import com.actionbarsherlock.view.MenuItem;
+import com.sherlock.navigationdrawer.compat.SherlockActionBarDrawerToggle;
+
 
 import de.keyboardsurfer.android.widget.crouton.Crouton;
 import de.keyboardsurfer.android.widget.crouton.Style;
@@ -53,12 +62,24 @@ LogoutConfirmationDialogFragment.LogoutConfirmationDialogListener {
     boolean networkAvailable;
     BroadcastReceiver networkReceiver;
     MenuItem searchItem;
+    
+    private DrawerLayout mDrawerLayout;
+    private ListView listView;
+    private SherlockActionBarDrawerToggle mDrawerToggle;
+    private ActionBarHelper mActionBar;
+    public static final String[] DRAWER_OPTIONS = 
+        {
+                "My List",   
+                "This Season",
+                "Most Popular"
+        };
+    
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         context = getApplicationContext();
-
+        
         mPrefManager = new PrefManager(context);
         init = mPrefManager.getInit();
 
@@ -71,6 +92,26 @@ LogoutConfirmationDialogFragment.LogoutConfirmationDialogListener {
             // Creates the adapter to return the Animu and Mango fragments
             mSectionsPagerAdapter = new HomeSectionsPagerAdapter(
                     getSupportFragmentManager());
+            
+            mDrawerLayout= (DrawerLayout)findViewById(R.id.drawer_layout);
+            listView = (ListView)findViewById(R.id.left_drawer);
+            
+            mDrawerLayout.setDrawerListener(new DemoDrawerListener());
+            mDrawerLayout.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START);
+            
+            listView.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, DRAWER_OPTIONS));
+    		listView.setOnItemClickListener(new DrawerItemClickListener());
+    		listView.setCacheColorHint(0);
+    		listView.setScrollingCacheEnabled(false);
+    		listView.setScrollContainer(false);
+    		listView.setFastScrollEnabled(true);
+    		listView.setSmoothScrollbarEnabled(true);
+            	
+    		mActionBar = createActionBarHelper();
+    		mActionBar.init();
+    		
+    		mDrawerToggle = new SherlockActionBarDrawerToggle(this, mDrawerLayout, R.drawable.ic_drawer_light, R.string.drawer_open, R.string.drawer_close);
+    		mDrawerToggle.syncState();
 
             mManager = new MALManager(context);
 
@@ -146,6 +187,9 @@ LogoutConfirmationDialogFragment.LogoutConfirmationDialogListener {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+    	if (mDrawerToggle.onOptionsItemSelected(item)) {
+			return true;
+		}
         switch (item.getItemId()) {
             case R.id.menu_settings:
                 startActivity(new Intent(this, Settings.class));
@@ -401,4 +445,82 @@ LogoutConfirmationDialogFragment.LogoutConfirmationDialogListener {
 
         supportInvalidateOptionsMenu();
     }
+    
+    /*private classes for nav drawer*/
+    private ActionBarHelper createActionBarHelper() {
+		return new ActionBarHelper();
+	}
+    
+    
+    private class DrawerItemClickListener implements ListView.OnItemClickListener {
+		@Override
+		public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+			/* do stuff when drawer item is clicked here */
+			mDrawerLayout.closeDrawer(listView);
+		}
+	}
+    
+    private class DemoDrawerListener implements DrawerLayout.DrawerListener {
+    	final ActionBar actionBar = getSupportActionBar();
+		@Override
+		public void onDrawerOpened(View drawerView) {
+			mDrawerToggle.onDrawerOpened(drawerView);
+			mActionBar.onDrawerOpened();
+		}
+
+		@Override
+		public void onDrawerClosed(View drawerView) {
+			mDrawerToggle.onDrawerClosed(drawerView);
+			mActionBar.onDrawerClosed();
+		}
+
+		@Override
+		public void onDrawerSlide(View drawerView, float slideOffset) {
+			mDrawerToggle.onDrawerSlide(drawerView, slideOffset);
+		}
+
+		@Override
+		public void onDrawerStateChanged(int newState) {
+			mDrawerToggle.onDrawerStateChanged(newState);
+		}
+	}
+    private class ActionBarHelper {
+		private final ActionBar mActionBar;
+		private CharSequence mDrawerTitle;
+		private CharSequence mTitle;
+
+		private ActionBarHelper() {
+			mActionBar = getSupportActionBar();
+		}
+
+		public void init() {
+			mActionBar.setDisplayHomeAsUpEnabled(true);
+			mActionBar.setHomeButtonEnabled(true);
+			mTitle = mDrawerTitle = getTitle();
+		}
+
+		/**
+		 * When the drawer is closed we restore the action bar state reflecting
+		 * the specific contents in view.
+		 */
+		public void onDrawerClosed() {
+			mActionBar.setTitle(mTitle);
+		}
+
+		/**
+		 * When the drawer is open we set the action bar to a generic title. The
+		 * action bar should only contain data relevant at the top level of the
+		 * nav hierarchy represented by the drawer, as the rest of your content
+		 * will be dimmed down and non-interactive.
+		 */
+		public void onDrawerOpened() {
+			mActionBar.setTitle(mDrawerTitle);
+		}
+
+		public void setTitle(CharSequence title) {
+			mTitle = title;
+		}
+	}
+    
+    
 }
