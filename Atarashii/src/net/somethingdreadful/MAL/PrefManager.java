ef5039396c6355aa26bfe9ae22edaa5f3a7ae334
ratan12 <ratandhawtal@hotmail.com>
2013-08-29 16:02:22 +0200

    Cleanup.

--- Atarashii/src/net/somethingdreadful/MAL/PrefManager.java
+++ Atarashii/src/net/somethingdreadful/MAL/PrefManager.java
@@ -18,84 +18,30 @@ public class PrefManager {
         prefeditor = prefs.edit();
     }
 
-    public boolean getInit()
-    {
-        init = prefs.getBoolean("init", false);
-
-        return init;
-    }
-
-    public String getUser()
-    {
+    public String getUser() {
         user = prefs.getString("user", "failed");
-
         return user;
     }
-
-    public boolean getUpgradeInit()
-    {
-        return prefs.getBoolean("upgradeInit", false);
-    }
-
-    public String getPass()
-    {
+    
+    public String getPass() {
         pass = prefs.getString("pass", "failed");
-
         return pass;
     }
 
-    public void setUser(String newUser)
-    {
-        prefeditor.putString("user", newUser);
+    public String getCustomShareText() {
+        return prefs.getString("customShareText", "Check out $title; on MyAnimeList!\n$link;");
     }
 
-    public void setPass(String newPass)
-    {
-        prefeditor.putString("pass", newPass);
+    public boolean getUpgradeInit() {
+        return prefs.getBoolean("upgradeInit", false);
     }
-
-    public void setInit(boolean newInit)
-    {
-        prefeditor.putBoolean("init", newInit);
+    
+    public boolean getInit() {
+        init = prefs.getBoolean("init", false);
+        return init;
     }
-
-    public void setUpgradeInit(boolean newUpgradeInit)
-    {
-        prefeditor.putBoolean("upgradeInit", newUpgradeInit);
-    }
-
-    public long getLastSyncTime()
-    {
-        long lastsync = 0;
-
-        lastsync = prefs.getLong("lastSync", 0);
-
-        return lastsync;
-    }
-
-    public void setLastSyncTime(long lastsync)
-    {
-        prefeditor.putLong("lastSync", lastsync);
-    }
-
-    public long getSyncFrequency()
-    {
-        long syncFrequency = 0;
-
-        syncFrequency = Long.parseLong(prefs.getString("syncFrequency", "604800000"));
-
-        return syncFrequency;
-    }
-
-    public int getDefaultList()
-    {
-        int defList = Integer.parseInt(prefs.getString("defList", "1"));
-
-        return defList;
-
-    }
-
-    public boolean getTraditionalListEnabled(){
+    
+    public boolean getTraditionalListEnabled() {
         return prefs.getBoolean("traditionalList", false);
     }
 
@@ -111,14 +57,44 @@ public class PrefManager {
         return prefs.getBoolean("Only_wifi", false);
     }
 
-    public String getCustomShareText()
-    {
-        return prefs.getString("customShareText", "Check out $title; on MyAnimeList!\n$link;");
+    public void setUser(String newUser) {
+        prefeditor.putString("user", newUser);
     }
 
-    public void commitChanges()
-    {
+    public void setPass(String newPass) {
+        prefeditor.putString("pass", newPass);
+    }
+
+    public void setInit(boolean newInit) {
+        prefeditor.putBoolean("init", newInit);
+    }
+
+    public void setUpgradeInit(boolean newUpgradeInit) {
+        prefeditor.putBoolean("upgradeInit", newUpgradeInit);
+    }
+
+    public void setLastSyncTime(long lastsync) {
+        prefeditor.putLong("lastSync", lastsync);
+    }
+    
+    public void commitChanges() {
         prefeditor.commit();
     }
 
+    public long getSyncFrequency() {
+        long syncFrequency = 0;
+        syncFrequency = Long.parseLong(prefs.getString("syncFrequency", "604800000"));
+        return syncFrequency;
+    }
+    
+    public long getLastSyncTime() {
+        long lastsync = 0;
+        lastsync = prefs.getLong("lastSync", 0);
+        return lastsync;
+    }
+
+    public int getDefaultList() {
+        int defList = Integer.parseInt(prefs.getString("defList", "1"));
+        return defList;
+    }
 }
