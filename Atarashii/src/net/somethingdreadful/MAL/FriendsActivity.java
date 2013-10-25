09b121298f4c81d7fc89f739621e5e72237fc55d
ratan12 <ratandhawtal@hotmail.com>
2013-10-25 20:15:39 +0200

    Added support for friendlist 3.0 atarashii api

--- Atarashii/src/net/somethingdreadful/MAL/FriendsActivity.java
+++ Atarashii/src/net/somethingdreadful/MAL/FriendsActivity.java
@@ -11,12 +11,10 @@ import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
 import org.json.JSONObject;
 
-import android.app.AlertDialog;
 import android.content.Context;
-import android.content.DialogInterface;
-import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.net.ConnectivityManager;
@@ -24,15 +22,11 @@ import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.text.InputType;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
-import android.widget.AdapterView.OnItemLongClickListener;
 import android.widget.BaseAdapter;
-import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -49,7 +43,7 @@ import de.keyboardsurfer.android.widget.crouton.Style;
 public class FriendsActivity extends SherlockFragmentActivity {
 	
     Context context;
-    ArrayList<String> UsernameList;
+    public static ArrayList<String> UsernameList;
     ListViewAdapter listadapter;
     int indexp; //position
     String text; //get selected username long
@@ -70,12 +64,9 @@ public class FriendsActivity extends SherlockFragmentActivity {
         setTitle("My friends"); //set title
         UsernameList = new ArrayList<String>();
         userList = (ListView)findViewById(R.id.listview);
-        
         listadapter = new ListViewAdapter();
-        
         restorelist(true); //restore users from preferences
         refresh(false);
-        clicklistener();
     }
 	
     public void remove(){ //removes a user
@@ -104,7 +95,7 @@ public class FriendsActivity extends SherlockFragmentActivity {
 			userList.setAdapter(listadapter);	
 			listadapter.notifyDataSetChanged();
         }catch(Exception e){
-        	maketext("You have no friends at your list!", 3);
+        	maketext("You have no friends at your list or refresh the list!", 3);
         }
     }
     
@@ -122,15 +113,8 @@ public class FriendsActivity extends SherlockFragmentActivity {
     		maketext("Error while restoring the list!", 2);
     		e.printStackTrace();
     	}
-    	
-    	try{// if the mal api is down this part will only crash.
-    		if (ProfileMALRecord.parse && onlywifi && isConnectedWifi() && R || network && ProfileMALRecord.parse && !onlywifi || network && forcesync && R){
-    			for(int position=0;position<size;position++){
-    				new Startparse().execute(UsernameList.get(position));
-    			}
-    		}
-    	}catch (Exception e){
-    		maketext("Atarashii could not receive the profile!", 2);
+    	if (ProfileMALRecord.parse && onlywifi && isConnectedWifi() && R || network && ProfileMALRecord.parse && !onlywifi || network && forcesync && R){
+    		new Startparse().execute(prefs.getUser());
     	}
     }
     
@@ -156,9 +140,6 @@ public class FriendsActivity extends SherlockFragmentActivity {
 		case android.R.id.home:
 			finish();
 			break;
-		case R.id.action_addToList:
-			inputdialog();
-			break;
 		case R.id.forceSync:
 			if (isNetworkAvailable()){
 				maketext("Sync started!", 1);
@@ -171,46 +152,6 @@ public class FriendsActivity extends SherlockFragmentActivity {
 			break;
 		}
         return super.onOptionsItemSelected(item);
-    }
-	
-    public class Verify extends AsyncTask<String, Void, String> { //check username
-    	protected String doInBackground(String... urls) {
-    		ProfileMALRecord.Clearrecord(false);
-    			HttpClient client = new DefaultHttpClient();
-    			String json = "";
-    			try {
-    				String line = "";
-    				HttpGet request = new HttpGet(urls[0]);
-    				HttpResponse response = client.execute(request);//get response
-    				BufferedReader rd = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
-    				while ((line = rd.readLine()) != null) {
-    					json += line + System.getProperty("line.separator"); //save response
-    					JSONObject jsonObject = new JSONObject(json);
-    					ProfileMALRecord.username = text;
-    					ProfileMALRecord.Grabrecord(jsonObject); //send the object to ProfileMALRecord
-    				}
-    			} catch (Exception e) {
-    				e.printStackTrace();
-    			}
-            return ""; // Avatar == "" or Avatar == the avatar URL
-        }
-
-        protected void onProgressUpdate(Void... progress) {
-        }
-
-		protected void onPostExecute(String check) { //check = avatar
-			if (ProfileMALRecord.avatar_url =="" && isNetworkAvailable()){ //checks if avatar == "" (This method also works when the api is offline)
-				maketext("Invailid username!",2);
-			}else if (check =="" && !isNetworkAvailable()){
-				maketext("No network connection available!",2);
-			}else if (!UsernameList.contains(text)){
-				UsernameList.add(text); //user exist, add the user
-				ProfileMALRecord.Saverecord();
-				refresh(true); //refresh the list
-			}else if (UsernameList.contains(text)){
-				maketext("User is already in your list!",1);
-			}
-        }
     }
 	
 	public void maketext(String string ,int type) { //for the private class, to make still text on errors...
@@ -233,72 +174,6 @@ public class FriendsActivity extends SherlockFragmentActivity {
         }
     }
 	
-	void removedialog(String text){ //before removing a user, show a alert
-		AlertDialog.Builder builder = new AlertDialog.Builder(this);
-		builder.setTitle("Remove " + text + " from my friendlist?");
-		builder.setMessage("Are you sure that you want to delete "+ text + " from my friendlist?");
-
-		builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() { 
-		    @Override
-		    public void onClick(DialogInterface dialog, int which) {
-		        remove();//remove user
-		    }
-		});
-		builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
-		    @Override
-		    public void onClick(DialogInterface dialog, int which) {
-		        dialog.cancel();
-		        maketext("Userprofile not Removed!", 1);
-		    }
-		});
-		builder.show();
-	}
-	
-	void inputdialog(){ //as the name says
-		AlertDialog.Builder builder = new AlertDialog.Builder(this);
-		builder.setTitle("Username of friend?");
-
-		final EditText input = new EditText(this);
-
-		input.setInputType(InputType.TYPE_CLASS_TEXT);
-		builder.setView(input);
-
-		builder.setPositiveButton("OK", new DialogInterface.OnClickListener() { 
-		    @Override
-		    public void onClick(DialogInterface dialog, int which) {
-		        String m_Text = input.getText().toString();
-		        text = m_Text;
-		        new Verify().execute("http://mal-api.com/profile/" + m_Text); // send url to the background
-		    }
-		});
-		builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-		    @Override
-		    public void onClick(DialogInterface dialog, int which) {
-		        dialog.cancel();
-		        maketext("Userprofile not added!", 2);
-		    }
-		});
-		builder.show();
-	}
-	void clicklistener(){
-		userList.setOnItemClickListener(new OnItemClickListener(){ //start the profile with your friend
-       	 public void onItemClick(AdapterView<?> arg0, View v,int position, long arg3){   
-       		 		ProfileMALRecord.parse = false;
-       		 		ProfileMALRecord.username =  UsernameList.get(position);
-       		 		ProfileMALRecord.Clearrecord(true);
-	        		Intent profile = new Intent(context, net.somethingdreadful.MAL.ProfileActivity.class);
-	 				startActivity(profile);
-            }
-        });
-        userList.setOnItemLongClickListener(new OnItemLongClickListener(){ //longclick = remove selected friend
-       	 public boolean onItemLongClick(AdapterView<?> arg0, View v, int position, long arg3){
-       		 ProfileMALRecord.username = UsernameList.get(position);
-       		 removedialog(ProfileMALRecord.username); //show confirm dialog
-       		 indexp = position;
-       		 return true;
-       	 }
-        });
-	}
 	  class ListViewAdapter extends BaseAdapter {
 		  
 	        public int getCount() {
@@ -325,25 +200,29 @@ public class FriendsActivity extends SherlockFragmentActivity {
 	            		view = inflater.inflate(R.layout.list_friends_with_text_item, parent, false);
 	                
 	            		//Set username
-	            		ProfileMALRecord.username =  UsernameList.get(position).toString();
-	            		ProfileMALRecord.Clearrecord(true);
+	            		String username =  UsernameList.get(position).toString();
 	            		TextView Username = (TextView) view.findViewById(R.id.userName);
-	            		Username.setText(ProfileMALRecord.username);
-	            		if (ProfileMALRecord.Developerrecord(ProfileMALRecord.username)) {
+	            		Username.setText(username);
+	            		if (ProfileMALRecord.Developerrecord(username)) {
 	            			Username.setTextColor(Color.parseColor("#8CD4D3")); //Developer
 	            		}
-	            		
+	            		SharedPreferences Read = PreferenceManager.getDefaultSharedPreferences(context);
+	            		String last_online = Read.getString("last_online" + username, "Unknown");
 	            		//Set online or offline status
 	            		TextView Status = (TextView) view.findViewById(R.id.status);
-	            		if (ProfileMALRecord.last_online.equals("Now")){
+	            		if (last_online.equals("Now")){
 	            			Status.setText("Online");
 	            			Status.setTextColor(Color.parseColor("#0D8500"));
 	            		}else{
 	            			Status.setText("Offline");
 	            			Status.setTextColor(Color.parseColor("#D10000"));
 	            		}
+	            		TextView since = (TextView) view.findViewById(R.id.since);
+	            		since.setText(Read.getString("since" + username, "Unknown"));
+	            		TextView lastonline = (TextView) view.findViewById(R.id.lastonline);
+	            		lastonline.setText(Read.getString("last_online" + username, "Unknown"));
 	            		Picasso picasso =  Picasso.with(context);
-		            	picasso.load(ProfileMALRecord.avatar_url).error(R.drawable.cover_error).placeholder(R.drawable.cover_loading).fit().into((ImageView) view.findViewById(R.id.profileImg));
+		            	picasso.load(Read.getString("avatar_url" + username, "http://cdn.myanimelist.net/images/na.gif")).error(R.drawable.cover_error).placeholder(R.drawable.cover_loading).fit().into((ImageView) view.findViewById(R.id.profileImg));
 	            	}
 	            }catch (Exception e){
 	            	e.printStackTrace();
@@ -351,7 +230,7 @@ public class FriendsActivity extends SherlockFragmentActivity {
 	            return view;
 	        }
 	  }
-	 
+	  
 	  public class Startparse extends AsyncTask<String, Void, String> {
 		  protected String doInBackground(String... urls) {
 			  if (isNetworkAvailable() && forcesync || isNetworkAvailable() && ProfileMALRecord.parse){ // settings check
@@ -359,19 +238,31 @@ public class FriendsActivity extends SherlockFragmentActivity {
 				  String json = "";
 				  try {
 					  String line = "";
-					  HttpGet request = new HttpGet("http://mal-api.com/profile/" + urls[0]);
+					  HttpGet request = new HttpGet("http://atarashii.yzi.me/profile/friends/" + urls[0]);
 					  HttpResponse response = client.execute(request);//get response
 					  BufferedReader rd = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+					  
 					  while ((line = rd.readLine()) != null) {
 						  json += line + System.getProperty("line.separator"); //save response
-						  JSONObject jsonObject = new JSONObject(json);
-						  ProfileMALRecord.username = urls[0];
-						  ProfileMALRecord.Clearrecord(true);
-						  ProfileMALRecord.Grabrecord(jsonObject); //send the object to ProfileMALRecord
-						  ProfileMALRecord.Saverecord();
+						  json = json.substring(0,json.length()-32);
+						  JSONArray jsonArray = new JSONArray(json);
+						  
+						  int length = jsonArray.length() - 1;
+						  UsernameList.clear();
+						  for(int lengtarray=0;lengtarray<=length;lengtarray++){
+							  JSONObject jsonObject = jsonArray.getJSONObject(lengtarray);
+							  SharedPreferences.Editor Edit = PreferenceManager.getDefaultSharedPreferences(context).edit();
+							  String user = jsonObject.getString("name");
+							  Edit.putString("last_online" + user ,jsonObject.getString("last_online")).commit();
+							  Edit.putString("since" + user ,jsonObject.getString("since")).commit();
+							  Edit.putString("avatar_url_short" + user ,jsonObject.getString("image_url_short")).commit();
+							  Edit.putString("avatar_url" + user ,jsonObject.getString("image_url")).commit();
+							  UsernameList.add(user);
+							  Log.e(user, Integer.toString(jsonArray.length()));
+						  }
 					  }
 				  } catch (Exception e) {
-					  e.printStackTrace();
+					  Log.e("FriendsActivity", "Error while parsing/receiving the list!");
 				  }
 			  }
 			  return "";
@@ -381,7 +272,7 @@ public class FriendsActivity extends SherlockFragmentActivity {
 		  }
 
 		  protected void onPostExecute(String bind) {
-			  refresh(false);
+			  refresh(true);
 		  }
 	  }
 }
