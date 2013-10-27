a6eed863f9b51a6318bd01029df21b8bc522dc71
ratan12 <ratandhawtal@hotmail.com>
2013-10-27 16:40:09 +0100

    Update FriendsActivity.java

--- Atarashii/src/net/somethingdreadful/MAL/FriendsActivity.java
+++ Atarashii/src/net/somethingdreadful/MAL/FriendsActivity.java
@@ -15,6 +15,7 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.net.ConnectivityManager;
@@ -26,6 +27,8 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
@@ -67,6 +70,15 @@ public class FriendsActivity extends SherlockFragmentActivity {
         listadapter = new ListViewAdapter();
         restorelist(false); //restore users from preferences
         refresh(false);
+        
+        userList.setOnItemClickListener(new OnItemClickListener(){ //start the profile with your friend
+		@Override
+		public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,long arg3) {
+    		ProfileMALRecord.username = UsernameList.get(arg2);
+    		Intent profile = new Intent(context, net.somethingdreadful.MAL.ProfileActivity.class);
+			startActivity(profile);
+		}
+        });
     }
 	
     public void remove(){ //removes a user
