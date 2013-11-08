5a996b5c28f5f9ff491cb3288ffc11bc09ff0bbf
d-sko <use.this@for.spam>
2013-11-08 19:26:05 +0100

    MALApi rewrite

--- Atarashii/src/net/somethingdreadful/MAL/api/MALApi.java
+++ Atarashii/src/net/somethingdreadful/MAL/api/MALApi.java
@@ -1,231 +1,132 @@
 package net.somethingdreadful.MAL.api;
 
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.HashMap;
-import java.util.Map.Entry;
+import java.util.List;
 
 import net.somethingdreadful.MAL.PrefManager;
+import net.somethingdreadful.MAL.api.response.Anime;
+import net.somethingdreadful.MAL.api.response.AnimeList;
+import net.somethingdreadful.MAL.api.response.Manga;
+import net.somethingdreadful.MAL.api.response.MangaList;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
+import retrofit.RestAdapter;
+import retrofit.client.Response;
 
 import android.content.Context;
+import android.os.Build;
 import android.util.Log;
 
-public class MALApi extends BaseMALApi {
+public class MALApi {
 	private static final String TAG = MALApi.class.getSimpleName();
-	private static String api_host = "http://api.atarashiiapp.com";
-
-	public MALApi(String username, String password) {
-		super(username, password);
-	}
+	private static final String API_HOST = "http://api.atarashiiapp.com";
+	public final static String USER_AGENT = "Atarashii! (Linux; Android " + Build.VERSION.RELEASE + "; " + Build.MODEL + " Build/" + Build.DISPLAY + ")";
+    
+    private MALInterface service;
+    private String username;
+    
+    public enum ListType {
+    	ANIME, MANGA
+    }
 
 	public MALApi(Context context) {
-		super(null, null);
 		PrefManager prefManager = new PrefManager(context);
-		setUsername(prefManager.getUser());
-		setPassword(prefManager.getPass());
+		username = prefManager.getUser();
+		setupRESTService(prefManager.getUser(), prefManager.getPass());
+	}
+	
+	public MALApi(String username, String password) {
+		this.username = username;
+		setupRESTService(username, password);
+	}
+	
+	private void setupRESTService(String username, String password) {
+		RestAdapter restAdapter = new RestAdapter.Builder()
+			.setServer(API_HOST)
+			.setRequestInterceptor(new MALRequestInterceptor(username, password))
+			.build();
+		service = restAdapter.create(MALInterface.class);
 	}
 
-	public JSONArray responseToJSONArray(RestResult<String> response) {
-		JSONArray result = null;
-
-		try {
-			result = new JSONArray(response.result);
-		} catch (JSONException e) {
-			Log.e(TAG, Log.getStackTraceString(e));
-		}
-		return result;
-
-	}
-
-	private static String getFullPath(String path) {
-		if (!path.startsWith("/")) {
-			path = "/" + path;
-		}
-		return MALApi.api_host + path;
-	}
-
-	private String encodeAsFormPost(HashMap<String, String> data) {
-		StringBuffer encodedData = new StringBuffer();
-		if (data != null) {
-
-			for (Entry<String, String> entry : data.entrySet()) {
-				encodedData.append(String.format("%s=%s&", entry.getKey(),
-						entry.getValue()));
-			}
-
-		}
-		encodedData.deleteCharAt(encodedData.length() - 1);
-		return encodedData.toString();
-	}
-
-	@Override
 	public boolean isAuth() {
-		URL url;
-		try {
-			url = new URL(getFullPath("account/verify_credentials"));
-
-			RestResult<String> response = restHelper.get(url);
-			return response != null && response.code == 200;
-		} catch (MalformedURLException e) {
-			Log.e(TAG, "", e);
-			return false;
-		}
+		Response response = service.verifyAuthentication();
+		return response.getStatus() == 200;
+	}
+	
+	public static ListType getListTypeByString(String name) {
+		return ListType.valueOf(name.toUpperCase());
+	}
+	
+	public static String getListTypeString(ListType type) {
+		return type.name().toLowerCase();
 	}
 
-	@Override
-	public JSONArray search(ListType listType, String query) {
-		URL url;
-		RestResult<String> response = null;
-		try {
-			url = new URL(getFullPath(getListTypeString(listType)
-					+ String.format("/search?q=%s", query)));
-			response = restHelper.get(url);
-		} catch (MalformedURLException e) {
-			Log.e(TAG, "Something went wrong, returning an empty list instead of null", e);
-			response = new RestResult<String>();
-		}
-		return responseToJSONArray(response);
+	public List<Anime> searchAnime(String query) {
+		return service.searchAnime(query);
+	}
+	
+	public List<Manga> searchManga(String query) {
+		return service.searchManga(query);
 	}
 
-	@Override
-	public JSONArray getList(ListType listType) {
-		JSONArray jsonArray = null;
-		try {
-			URL url = new URL(getFullPath(getListTypeString(listType) + "list/"
-					+ getUsername()));
-			RestResult<String> response = restHelper.get(url);
-
-			if (response != null) {
-				jsonArray = new JSONObject(response.result)
-						.getJSONArray(getListTypeString(listType));
-			}
-		} catch (JSONException e) {
-			Log.e(TAG, Log.getStackTraceString(e));
-
-		} catch (MalformedURLException e) {
-			Log.e(TAG, "", e);
-		}
-		return jsonArray;
+	public AnimeList getAnimeList() {
+		return service.getAnimeList(username);
+	}
+	
+	public MangaList getMangaList() {
+		return service.getMangaList(username);
 	}
 
-	@Override
-	public JSONObject getDetail(Integer id, ListType listType) {
-		JSONObject jsonObject = null;
-		try {
-			URL url = new URL(getFullPath(getListTypeString(listType) + "/"
-					+ id));
-			RestResult<String> response = restHelper.get(url);
-
-			if (response != null) {
-				jsonObject = new JSONObject(response.result);
-			}
-		} catch (JSONException e) {
-			Log.e(TAG, Log.getStackTraceString(e));
-
-		} catch (MalformedURLException e) {
-			Log.e(TAG, "", e);
-		}
-		return jsonObject;
+	public Anime getAnime(int id) {
+		return service.getAnime(id);
+	}
+	
+	public Manga getManga(int id) {
+		return service.getManga(id);
 	}
 
-	@Override
-	public boolean addOrUpdateGenreInList(boolean hasCreate, ListType listType,
-			String genre_id, HashMap<String, String> data) {
-		String listPrefix = getListTypeString(listType);
-		String uri = getFullPath(listPrefix + "list" + "/" + listPrefix);
-		RestResult<String> response = null;
-		try {
-			if (!hasCreate) {
-				uri += "/" + genre_id;
-				response = restHelper.put(new URL(uri), encodeAsFormPost(data));
-
-			} else {
-				data = new HashMap<String, String>(data);
-				data.put(listPrefix + "_id", genre_id);
-				response = restHelper.post(new URL(uri), encodeAsFormPost(data));
-			}
-		} catch (MalformedURLException e) {
-			Log.e(TAG, "", e);
-		}
-
-		return response.code == 200;
+	public boolean addOrUpdateAnime(boolean hasCreate, String id, Anime data) {
+		// TODO
+		return false;
+	}
+	
+	public boolean addOrUpdateManga(boolean hasCreate, String id, Manga data) {
+		// TODO
+		return false;
 	}
 
-	@Override
 	public boolean deleteGenreFromList(ListType listType, String genre_id) {
-		String listPrefix = getListTypeString(listType);
-		URL url;
-		RestResult<String> response = null;
-		try {
-			url = new URL(getFullPath(listPrefix + "list" + "/" + listPrefix
-					+ "/" + genre_id));
-			response = restHelper.delete(url);
-		} catch (MalformedURLException e) {
-			Log.e(TAG, "", e);
-			return false;
-		}
-		return response.code == 200;
+		// TODO
+		return false;
 	}
 	
-	@Override
-	public JSONArray getMostPopular(ListType listType, int page){
-		URL url;
-		RestResult<String> response = null;
-		try {
-			url = new URL(getFullPath(getListTypeString(listType) + "/popular?page="+page));
-			response = restHelper.get(url);
-		} catch (Exception e) {
-			Log.e(TAG, "Something went wrong, returning an empty list instead of null", e);
-			response = new RestResult<String>();
-		}
-		return responseToJSONArray(response);
+	public List<Anime> getMostPopularAnime(int page) {
+		return service.getPopularAnime(page);
 	}
 	
-	@Override
-	public JSONArray getTopRated(ListType listType, int page){
-		URL url;
-		RestResult<String> response = null;
-		try {
-			url = new URL(getFullPath(getListTypeString(listType) + "/top?page="+page));
-			response = restHelper.get(url);
-		} catch (Exception e) {
-			Log.e(TAG, "Something went wrong, returning an empty list instead of null", e);
-			response = new RestResult<String>();
-		}
-		return responseToJSONArray(response);
+	public List<Manga> getMostPopularManga(int page) {
+		return service.getPopularManga(page);
 	}
 	
-	@Override
-	public JSONArray getJustAdded(ListType listType, int page){
-		URL url;
-		RestResult<String> response = null;
-		try {
-			url = new URL(getFullPath(getListTypeString(listType) + "/just_added?page="+page));
-			response = restHelper.get(url);
-		} catch (Exception e) {
-			Log.e(TAG, "Something went wrong, returning an empty list instead of null", e);
-			response = new RestResult<String>();
-		}
-		return responseToJSONArray(response);
+	public List<Anime> getTopRatedAnime(int page) {
+		return service.getTopRatedAnime(page);
 	}
 	
-	@Override
-	public JSONArray getUpcoming(ListType listType, int page){
-		URL url;
-		RestResult<String> response = null;
-		try {
-			url = new URL(getFullPath(getListTypeString(listType) + "/upcoming?page="+page));
-			response = restHelper.get(url);
-		} catch (Exception e) {
-			Log.e(TAG, "Something went wrong, returning an empty list instead of null", e);
-			response = new RestResult<String>();
-		}
-		return responseToJSONArray(response);
+	public List<Manga> getTopRatedManga(int page) {
+		return service.getTopRatedManga(page);
 	}
 	
-
+	public List<Anime> getJustAddedAnime(int page) {
+		return service.getJustAddedAnime(page);
+	}
+	
+	public List<Manga> getJustAddedManga(int page) {
+		return service.getJustAddedManga(page);
+	}
+	
+	public List<Anime> getUpcomingAnime(int page) {
+		return service.getUpcomingAnime(page);
+	}
+	
+	public List<Manga> getUpcomingManga(int page) {
+		return service.getUpcomingManga(page);
+	}
 }
--- Atarashii/src/net/somethingdreadful/MAL/api/MALInterface.java
+++ Atarashii/src/net/somethingdreadful/MAL/api/MALInterface.java
@@ -1,20 +1,46 @@
 package net.somethingdreadful.MAL.api;
 
+import java.util.List;
+
 import net.somethingdreadful.MAL.api.response.Anime;
 import net.somethingdreadful.MAL.api.response.AnimeList;
 import net.somethingdreadful.MAL.api.response.Manga;
 import net.somethingdreadful.MAL.api.response.MangaList;
+import retrofit.client.Response;
 import retrofit.http.GET;
 import retrofit.http.Path;
 
 public interface MALInterface {
+	@GET("/account/verify_credentials")
+	Response verifyAuthentication();
+
 	@GET("/anime/{anime_id}")
 	Anime getAnime(@Path("anime_id") int anime_id);
+	@GET("/anime/search?q={query}")
+	List<Anime> searchAnime(@Path("query") String query);
+	@GET("/anime/popular?page={page}")
+	List<Anime> getPopularAnime(@Path("page") int page);
+	@GET("/anime/top?page={page}")
+	List<Anime> getTopRatedAnime(@Path("page") int page);
+	@GET("/anime/upcoming?page={page}")
+	List<Anime> getUpcomingAnime(@Path("page") int page);
+	@GET("/anime/just_added?page={page}")
+	List<Anime> getJustAddedAnime(@Path("page") int page);
 	@GET("/animelist/{username}")
 	AnimeList getAnimeList(@Path("username") String username);
 	
 	@GET("/manga/{manga_id}")
 	Manga getManga(@Path("manga_id") int manga_id);
+	@GET("/manga/search?q={query}")
+	List<Manga> searchManga(@Path("query") String query);
+	@GET("/manga/popular?page={page}")
+	List<Manga> getPopularManga(@Path("page") int page);
+	@GET("/manga/top?page={page}")
+	List<Manga> getTopRatedManga(@Path("page") int page);
+	@GET("/manga/upcoming?page={page}")
+	List<Manga> getUpcomingManga(@Path("page") int page);
+	@GET("/manga/just_added?page={page}")
+	List<Manga> getJustAddedManga(@Path("page") int page);
 	@GET("/mangalist/{username}")
 	MangaList getMangaList(@Path("username") String username);
 }
