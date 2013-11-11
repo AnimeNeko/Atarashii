ec076e040f1743090386d50a9e31f3ce9c581276
d-sko <use.this@for.spam>
2013-11-11 21:40:53 +0100

    fixed indents

--- Atarashii/src/net/somethingdreadful/MAL/sql/DatabaseManager.java
+++ Atarashii/src/net/somethingdreadful/MAL/sql/DatabaseManager.java
@@ -12,18 +12,18 @@ import android.util.Log;
 
 public class DatabaseManager {
 
-    static MALSqlHelper malSqlHelper;
+	static MALSqlHelper malSqlHelper;
 
 	public DatabaseManager(Context context) {
 		if (malSqlHelper == null) {
-            malSqlHelper = MALSqlHelper.getHelper(context);
-        }
+			malSqlHelper = MALSqlHelper.getHelper(context);
+		}
 	}
-	
+
 	public synchronized static SQLiteDatabase getDBWrite() {
-        return malSqlHelper.getWritableDatabase();
-    }
-	
+		return malSqlHelper.getWritableDatabase();
+	}
+
 	public void saveAnimeList(ArrayList<Anime> list) {
 		if ( list != null && list.size() > 0 ) {
 			try {
@@ -38,30 +38,30 @@ public class DatabaseManager {
 			}
 		}
 	}
-	
+
 	public void saveAnime(Anime anime, boolean ignoreSynopsis) {
 		ContentValues cv = new ContentValues();
 
-        cv.put("recordID", anime.getId());
-        cv.put("recordName", anime.getTitle());
-        cv.put("recordType", anime.getType());
-        cv.put("imageUrl", anime.getImageUrl());
-        cv.put("recordStatus", anime.getStatus());
-        cv.put("myStatus", anime.getWatchedStatus());
-        cv.put("memberScore", anime.getMembersScore());
-        cv.put("myScore", anime.getScore());
-        cv.put("episodesWatched", anime.getWatchedEpisodes());
-        cv.put("episodesTotal", anime.getEpisodes());
-        cv.put("dirty", anime.getDirty());
-        cv.put("lastUpdate", anime.getLastUpdate().toString());
+		cv.put("recordID", anime.getId());
+		cv.put("recordName", anime.getTitle());
+		cv.put("recordType", anime.getType());
+		cv.put("imageUrl", anime.getImageUrl());
+		cv.put("recordStatus", anime.getStatus());
+		cv.put("myStatus", anime.getWatchedStatus());
+		cv.put("memberScore", anime.getMembersScore());
+		cv.put("myScore", anime.getScore());
+		cv.put("episodesWatched", anime.getWatchedEpisodes());
+		cv.put("episodesTotal", anime.getEpisodes());
+		cv.put("dirty", anime.getDirty());
+		cv.put("lastUpdate", anime.getLastUpdate().toString());
 
-        if (!ignoreSynopsis) {
-            cv.put("synopsis", anime.getSynopsis());
-        }
-        
-        getDBWrite().replace(MALSqlHelper.TABLE_ANIME, null, cv);
+		if (!ignoreSynopsis) {
+			cv.put("synopsis", anime.getSynopsis());
+		}
+
+		getDBWrite().replace(MALSqlHelper.TABLE_ANIME, null, cv);
 	}
-	
+
 	public void saveMangaList(ArrayList<Manga> list) {
 		if ( list != null && list.size() > 0 ) {
 			try {
@@ -76,29 +76,29 @@ public class DatabaseManager {
 			}
 		}
 	}
-	
+
 	public void saveManga(Manga manga, boolean ignoreSynopsis) {
 		ContentValues cv = new ContentValues();
 
-        cv.put("recordID", manga.getId());
-        cv.put("recordName", manga.getTitle());
-        cv.put("recordType", manga.getType());
-        cv.put("imageUrl", manga.getImageUrl());
-        cv.put("recordStatus", manga.getStatus());
-        cv.put("myStatus", manga.getReadStatus());
-        cv.put("memberScore", manga.getMembersScore());
-        cv.put("myScore", manga.getScore());
-        cv.put("volumesRead", manga.getVolumesRead());
-        cv.put("chaptersRead", manga.getChaptersRead());
-        cv.put("volumesTotal", manga.getVolumes());
-        cv.put("chaptersTotal", manga.getChapters());
-        cv.put("dirty", manga.getDirty());
-        cv.put("lastUpdate", manga.getLastUpdate().toString());
+		cv.put("recordID", manga.getId());
+		cv.put("recordName", manga.getTitle());
+		cv.put("recordType", manga.getType());
+		cv.put("imageUrl", manga.getImageUrl());
+		cv.put("recordStatus", manga.getStatus());
+		cv.put("myStatus", manga.getReadStatus());
+		cv.put("memberScore", manga.getMembersScore());
+		cv.put("myScore", manga.getScore());
+		cv.put("volumesRead", manga.getVolumesRead());
+		cv.put("chaptersRead", manga.getChaptersRead());
+		cv.put("volumesTotal", manga.getVolumes());
+		cv.put("chaptersTotal", manga.getChapters());
+		cv.put("dirty", manga.getDirty());
+		cv.put("lastUpdate", manga.getLastUpdate().toString());
 
-        if (!ignoreSynopsis) {
-            cv.put("synopsis", manga.getSynopsis());
-        }
-        
-        getDBWrite().replace(MALSqlHelper.TABLE_MANGA, null, cv);
+		if (!ignoreSynopsis) {
+			cv.put("synopsis", manga.getSynopsis());
+		}
+
+		getDBWrite().replace(MALSqlHelper.TABLE_MANGA, null, cv);
 	}
 }
