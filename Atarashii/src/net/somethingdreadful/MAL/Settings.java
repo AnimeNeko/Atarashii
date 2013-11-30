ec7b43324f90347454c5ea2a0110ae1410ebb035
ratan12 <ratandhawtal@hotmail.com>
2013-11-30 22:09:13 +0100

    Android 2.2 bug fix

--- Atarashii/src/net/somethingdreadful/MAL/Settings.java
+++ Atarashii/src/net/somethingdreadful/MAL/Settings.java
@@ -1,7 +1,10 @@
 package net.somethingdreadful.MAL;
 
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceManager;
+import android.preference.PreferenceScreen;
+
 import com.actionbarsherlock.app.ActionBar;
 import com.actionbarsherlock.app.SherlockPreferenceActivity;
 import com.actionbarsherlock.view.MenuItem;
@@ -18,10 +21,9 @@ public class Settings extends SherlockPreferenceActivity {
 
         PreferenceManager prefMgr = getPreferenceManager();
         prefMgr.setSharedPreferencesName("prefs");
-
+        
         addPreferencesFromResource(R.xml.settings);
     }
-
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -30,6 +32,19 @@ public class Settings extends SherlockPreferenceActivity {
                 finish();
         }
         return true;
+    }
+    
+    //Fix for the android(2,2+) theme bug (https://code.google.com/p/android/issues/detail?id=4611)
+    @SuppressWarnings("deprecation")
+    @Override
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference)
+    {
+    	super.onPreferenceTreeClick(preferenceScreen, preference);
+    	if (preference!=null)
+	    	if (preference instanceof PreferenceScreen)
+	        	if (((PreferenceScreen)preference).getDialog()!=null)
+	        		((PreferenceScreen)preference).getDialog().getWindow().getDecorView().setBackgroundDrawable(this.getWindow().getDecorView().getBackground().getConstantState().newDrawable());
+    	return false;
     }
 
 }
