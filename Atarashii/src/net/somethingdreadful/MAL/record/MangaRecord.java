7cc6b7006cdbc5f9cd2a2241b53e6b960797a510
ratan12 <ratandhawtal@hotmail.com>
2013-12-23 20:11:45 +0100

    Fixed bug on specfied devices

--- Atarashii/src/net/somethingdreadful/MAL/record/MangaRecord.java
+++ Atarashii/src/net/somethingdreadful/MAL/record/MangaRecord.java
@@ -71,9 +71,6 @@ public class MangaRecord extends GenericMALRecord {
     }
 
     public static HashMap<String, Class<?>> getTypeMap() {
-        /* if (typeMap != null) {
-            return typeMap;
-        } */
         typeMap = GenericMALRecord.getTypeMap();
         typeMap.put("volumesTotal", Integer.class);
         typeMap.put("chaptersTotal", Integer.class);
