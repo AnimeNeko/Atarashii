d8710207660a2f5fa99a8a6b022c766fe0c6dbad
Michael Johnson <youngmug@animeneko.net>
2014-02-14 12:33:23 -0800

    Fix Crash with No NFC
    Since getDefaultAdapter returns null if there isn't an adapter in the
    device, only operate on it if it actually exists.
    
    Fixes #93

--- Atarashii/src/net/somethingdreadful/MAL/NfcHelper.java
+++ Atarashii/src/net/somethingdreadful/MAL/NfcHelper.java
@@ -11,8 +11,10 @@ public class NfcHelper {
     @TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
     public static void disableBeam(Activity activity) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
-            NfcAdapter adapter = NfcAdapter.getDefaultAdapter(activity); 
-            adapter.setNdefPushMessage(null, activity);
+            NfcAdapter adapter = NfcAdapter.getDefaultAdapter(activity);
+            if (adapter != null) {
+            	adapter.setNdefPushMessage(null, activity);
+            }
         }
     }
 }
