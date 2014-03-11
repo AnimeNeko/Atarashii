9aa0779b2b216778497792d82c71550480aed77e
Chris Fanti <animabolt@gmail.com>
2014-03-10 22:59:08 -0400

    If total is unknown, display "?"
    The current behaviour is we display a 0, which is what
    the API sends for unknown. That's pretty silly, so instead
    let's display a "?".
    
    Also my Eclipse autoformatting apparantly touched some other lines.
    Meh.

--- Atarashii/src/net/somethingdreadful/MAL/DetailView.java
+++ Atarashii/src/net/somethingdreadful/MAL/DetailView.java
@@ -6,9 +6,6 @@ import net.somethingdreadful.MAL.api.MALApi;
 import net.somethingdreadful.MAL.api.response.Anime;
 import net.somethingdreadful.MAL.api.response.GenericRecord;
 import net.somethingdreadful.MAL.api.response.Manga;
-
-import org.apache.commons.lang3.text.WordUtils;
-
 import android.annotation.TargetApi;
 import android.content.Context;
 import android.content.Intent;
@@ -26,7 +23,6 @@ import android.support.v4.app.FragmentTransaction;
 import android.text.Html;
 import android.text.Spanned;
 import android.text.method.LinkMovementMethod;
-import android.text.method.MovementMethod;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -125,7 +121,7 @@ RemoveConfirmationDialogFragment.RemoveConfirmationDialogListener {
         SynopsisFragment = (GenericCardFragment) fm.findFragmentById(R.id.SynopsisFragment);
         SynopsisFragment.setArgsSensibly(getString(R.string.card_name_synopsis), R.layout.card_layout_content_synopsis, GenericCardFragment.CONTENT_TYPE_SYNOPSIS, false);
         SynopsisFragment.inflateContentStub();
-        
+
         SynopsisView = (TextView) SynopsisFragment.getView().findViewById(R.id.SynopsisContent);
         SynopsisView.setMovementMethod(LinkMovementMethod.getInstance());
 
@@ -186,10 +182,10 @@ RemoveConfirmationDialogFragment.RemoveConfirmationDialogListener {
         // Set up the action bar.
         actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
-        
+
         setupBeam();
     }
-    
+
     @TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
     private void setupBeam() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
@@ -200,13 +196,13 @@ RemoveConfirmationDialogFragment.RemoveConfirmationDialogListener {
             } else {
                 // Register NFC callback
                 String message_str = recordType + ":" + String.valueOf(recordID);
-                NdefMessage message = new NdefMessage(new NdefRecord[] { 
-                    new NdefRecord(
-                        NdefRecord.TNF_MIME_MEDIA ,
-                        "application/net.somethingdreadful.MAL".getBytes(Charset.forName("US-ASCII")),
-                        new byte[0], message_str.getBytes(Charset.forName("US-ASCII"))
-                    ),
-                    NdefRecord.createApplicationRecord("net.somethingdreadful.MAL")
+                NdefMessage message = new NdefMessage(new NdefRecord[] {
+                        new NdefRecord(
+                                NdefRecord.TNF_MIME_MEDIA ,
+                                "application/net.somethingdreadful.MAL".getBytes(Charset.forName("US-ASCII")),
+                                new byte[0], message_str.getBytes(Charset.forName("US-ASCII"))
+                                ),
+                                NdefRecord.createApplicationRecord("net.somethingdreadful.MAL")
                 });
                 mNfcAdapter.setNdefPushMessage(message, this);
             }
@@ -227,15 +223,15 @@ RemoveConfirmationDialogFragment.RemoveConfirmationDialogListener {
 
     @Override
     public boolean onPrepareOptionsMenu(Menu menu) {
-    	if (isAdded) {
-    		if (networkAvailable) {
-        		menu.findItem(R.id.action_Remove).setVisible(true);
-        	}
-        	else {
-        		menu.findItem(R.id.action_Remove).setVisible(false);
-        	}
-    	}
-    	
+        if (isAdded) {
+            if (networkAvailable) {
+                menu.findItem(R.id.action_Remove).setVisible(true);
+            }
+            else {
+                menu.findItem(R.id.action_Remove).setVisible(false);
+            }
+        }
+
         return true;
 
     }
@@ -283,7 +279,7 @@ RemoveConfirmationDialogFragment.RemoveConfirmationDialogListener {
         if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(getIntent().getAction()))
             processIntent(getIntent());
     }
-    
+
     @TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
     private void processIntent(Intent intent) {
         if ( Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH ) {
@@ -391,7 +387,7 @@ RemoveConfirmationDialogFragment.RemoveConfirmationDialogListener {
         }
         mpdf.show(fm, "fragment_EditMangaProgressDialog");
     }
-    
+
     private String getStringFromResourceArray(int resArrayId, int notFoundStringId, int index) {
         Resources res = getResources();
         try {
@@ -404,11 +400,11 @@ RemoveConfirmationDialogFragment.RemoveConfirmationDialogListener {
             return res.getString(notFoundStringId);
         }
     }
-    
+
     private String getAnimeTypeString(int typeInt) {
         return getStringFromResourceArray(R.array.mediaType_Anime, R.string.unknown, typeInt);
     }
-    
+
     private String getAnimeStatusString(int statusInt) {
         return getStringFromResourceArray(R.array.mediaStatus_Anime, R.string.unknown, statusInt);
     }
@@ -416,7 +412,7 @@ RemoveConfirmationDialogFragment.RemoveConfirmationDialogListener {
     private String getMangaTypeString(int typeInt) {
         return getStringFromResourceArray(R.array.mediaType_Manga, R.string.unknown, typeInt);
     }
-    
+
     private String getMangaStatusString(int statusInt) {
         return getStringFromResourceArray(R.array.mediaStatus_Manga, R.string.unknown, statusInt);
     }
@@ -494,7 +490,14 @@ RemoveConfirmationDialogFragment.RemoveConfirmationDialogListener {
                 .into(CoverImageView);
 
                 ProgressText = Integer.toString(animeRecord.getWatchedEpisodes());
-                TotalProgressText = Integer.toString(animeRecord.getEpisodes());
+
+                if (animeRecord.getEpisodes() == 0) {
+                    TotalProgressText = "?";
+                }
+                else {
+                    TotalProgressText = Integer.toString(animeRecord.getEpisodes());
+                }
+
                 MyStatusText = getUserStatusString(animeRecord.getWatchedStatusInt());
 
                 ProgressCurrentView = (TextView) ProgressFragment.getView().findViewById(R.id.progressCountCurrent);
@@ -537,9 +540,23 @@ RemoveConfirmationDialogFragment.RemoveConfirmationDialogListener {
                 .into(CoverImageView);
 
                 VolumeProgressText = Integer.toString(mangaRecord.getVolumesRead());
-                VolumeTotalText = Integer.toString(mangaRecord.getVolumes());
+
+                if (mangaRecord.getVolumes() == 0) {
+                    VolumeTotalText = "?";
+                }
+                else {
+                    VolumeTotalText = Integer.toString(mangaRecord.getVolumes());
+                }
+
                 ProgressText = Integer.toString(mangaRecord.getChaptersRead());
-                TotalProgressText = Integer.toString(mangaRecord.getChapters());
+
+                if (mangaRecord.getChapters() == 0) {
+                    TotalProgressText = "?";
+                }
+                else {
+                    TotalProgressText = Integer.toString(mangaRecord.getChapters());
+                }
+
                 MyStatusText = getUserStatusString(mangaRecord.getReadStatusInt());
 
 
@@ -591,32 +608,32 @@ RemoveConfirmationDialogFragment.RemoveConfirmationDialogListener {
                 if ("anime".equals(internalType)) {
                     MALScoreBar = (RatingBar) ScoreFragment.getView().findViewById(R.id.MALScoreBar);
                     MyScoreBar = (RatingBar) ScoreFragment.getView().findViewById(R.id.MyScoreBar);
-    
+
                     if (MALScoreBar != null) {
                         MALScoreBar.setRating(MemberScore / 2);
                         MyScoreBar.setRating(MyScore / 2);
                     }
-    
+
                     MyStatusView = (TextView) WatchStatusFragment.getView().findViewById(R.id.cardStatusLabel);
-    
+
                     if (MyStatusView != null) {
                         MyStatusView.setText(MyStatusText);
                     }
-    
+
                 } else {
-    
+
                     MemberScore = mangaRecord.getMembersScore();
                     MyScore = mangaRecord.getScore();
-    
+
                     MALScoreBar = (RatingBar) ScoreFragment.getView().findViewById(R.id.MALScoreBar);
                     MyScoreBar = (RatingBar) ScoreFragment.getView().findViewById(R.id.MyScoreBar);
-    
+
                     if (MALScoreBar != null) {
                         MALScoreBar.setRating(MemberScore / 2);
                         MyScoreBar.setRating(MyScore / 2);
                     }
                 }
-    
+
                 if (gr.getSpannedSynopsis() != null) {
                     SynopsisText = gr.getSpannedSynopsis();
                     MemberScore = gr.getMembersScore();
@@ -625,16 +642,16 @@ RemoveConfirmationDialogFragment.RemoveConfirmationDialogListener {
                     SynopsisText = Html.fromHtml("<em>No data loaded.</em>");
                     MemberScore = 0.0f;
                 }
-    
+
                 if (SynopsisFragment.getView() != null) {
                     SynopsisView = (TextView) SynopsisFragment.getView().findViewById(R.id.SynopsisContent);
-    
+
                     if (SynopsisView != null) {
                         SynopsisView.setText(SynopsisText, TextView.BufferType.SPANNABLE);
-    
+
                     }
                 }
-    
+
                 if (MALScoreBar != null) {
                     MALScoreBar.setRating(MemberScore / 2);
                     MyScoreBar.setRating(MyScore / 2);
@@ -672,21 +689,21 @@ RemoveConfirmationDialogFragment.RemoveConfirmationDialogListener {
                 internalManager.saveMangaToDatabase((Manga) gr[0], false);
 
             if (gr[0].getDeleteFlag()) {
-            	if ("anime".equals(internalType)) {
+                if ("anime".equals(internalType)) {
                     internalManager.deleteAnimeFromDatabase((Anime) gr[0]);
                     result = internalManager.writeAnimeDetailsToMAL((Anime) gr[0]);
-            	} else {
+                } else {
                     internalManager.deleteMangaFromDatabase((Manga) gr[0]);
                     result = internalManager.writeMangaDetailsToMAL((Manga) gr[0]);
-            	}
+                }
             }
             else {
                 if (internalNetworkAvailable){
                     if ("anime".equals(internalType)) {
                         if (gr[0].getCreateFlag())
-                        	result = internalManager.addAnimeToMAL((Anime) gr[0]);
+                            result = internalManager.addAnimeToMAL((Anime) gr[0]);
                         else
-                        	result = internalManager.writeAnimeDetailsToMAL((Anime) gr[0]);
+                            result = internalManager.writeAnimeDetailsToMAL((Anime) gr[0]);
                     } else {
                         if (gr[0].getCreateFlag()) {
                             result = internalManager.addMangaToMAL((Manga) gr[0]);
