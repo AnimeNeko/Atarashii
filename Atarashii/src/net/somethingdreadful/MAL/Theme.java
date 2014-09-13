b42018a1a07e538602d11913095480b577f067d1
ratan12 <ratandhawtal@hotmail.com>
2014-09-13 18:28:24 +0200

    Fix language revert after boot (Language)

--- Atarashii/src/net/somethingdreadful/MAL/Theme.java
+++ Atarashii/src/net/somethingdreadful/MAL/Theme.java
@@ -1,9 +1,32 @@
 package net.somethingdreadful.MAL;
 
+import android.content.res.Configuration;
+
 import org.holoeverywhere.ThemeManager;
 import org.holoeverywhere.app.Application;
 
+import java.util.Locale;
+
 public class Theme extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        PrefManager Prefs = new PrefManager(getApplicationContext());
+        String localeName = Prefs.getlocale();
+        Locale locale;
+        if (localeName.equals("pt-br"))
+            locale = new Locale("pt", "PT");
+        else if (localeName.equals("pt-pt"))
+            locale = new Locale("pt", "BR");
+        else
+            locale = new Locale(Prefs.getlocale());
+        Locale.setDefault(locale);
+
+        Configuration newConfig = new Configuration();
+        newConfig.locale = locale;
+        getBaseContext().getResources().updateConfiguration(newConfig, getBaseContext().getResources().getDisplayMetrics());
+    }
+
     static {
         ThemeManager.setDefaultTheme(ThemeManager.MIXED);
     }
