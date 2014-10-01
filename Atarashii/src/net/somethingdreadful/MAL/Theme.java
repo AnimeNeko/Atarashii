396e106e665df476d0bc30812c14a37867765b3e
ratan12 <ratandhawtal@hotmail.com>
2014-10-01 18:17:49 +0200

    Fix language when the device has been rotated (Language)

--- Atarashii/src/net/somethingdreadful/MAL/Theme.java
+++ Atarashii/src/net/somethingdreadful/MAL/Theme.java
@@ -8,17 +8,30 @@ import org.holoeverywhere.app.Application;
 import java.util.Locale;
 
 public class Theme extends Application {
+
+    Locale locale;
+
     @Override
     public void onCreate() {
         super.onCreate();
         PrefManager Prefs = new PrefManager(getApplicationContext());
-        Locale locale = Prefs.getLocale();
+        locale = Prefs.getLocale();
 
         // apply it until the app remains cached
         Locale.setDefault(locale);
         Configuration newConfig = new Configuration();
         newConfig.locale = locale;
         getBaseContext().getResources().updateConfiguration(newConfig, getBaseContext().getResources().getDisplayMetrics());
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        // apply it until the app remains cached
+        Locale.setDefault(locale);
+        Configuration Config = new Configuration();
+        Config.locale = locale;
+        getBaseContext().getResources().updateConfiguration(Config, getBaseContext().getResources().getDisplayMetrics());
     }
 
     static {
