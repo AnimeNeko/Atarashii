6b19ab750dce9e04f8452804bc3a41a0ebcba0ba
ratan12 <ratandhawtal@hotmail.com>
2014-11-06 19:14:48 +0100

    Add translations info (About)

--- Atarashii/src/net/somethingdreadful/MAL/AboutActivity.java
+++ Atarashii/src/net/somethingdreadful/MAL/AboutActivity.java
@@ -19,6 +19,7 @@ public class AboutActivity extends Activity {
         ((Card) findViewById(R.id.atarashii_card)).setContent(R.layout.card_about_atarashii);
         ((Card) findViewById(R.id.contributors_card)).setContent(R.layout.card_about_contributors);
         ((Card) findViewById(R.id.community_card)).setContent(R.layout.card_about_community);
+        ((Card) findViewById(R.id.translations_card)).setContent(R.layout.card_about_translations);
         ((Card) findViewById(R.id.acknowledgements_card)).setContent(R.layout.card_about_acknowledgements);
         TextView animaMalContent = (TextView) findViewById(R.id.contributor_anima_name);
         TextView motokochanMalContent = (TextView) findViewById(R.id.contributor_motokochan_name);
@@ -27,6 +28,8 @@ public class AboutActivity extends Activity {
         TextView ratan12MalContent = (TextView) findViewById(R.id.contributor_ratan12_name);
         TextView acknowledgementsContent = (TextView) findViewById(R.id.acknowledgements_card_content);
         TextView communityContent = (TextView) findViewById(R.id.community_card_content);
+        TextView translationsContent = (TextView) findViewById(R.id.translations_card_content);
+        TextView notListedContent = (TextView) findViewById(R.id.notlisted_content);
 
         animaMalContent.setMovementMethod(LinkMovementMethod.getInstance());
         motokochanMalContent.setMovementMethod(LinkMovementMethod.getInstance());
@@ -35,6 +38,8 @@ public class AboutActivity extends Activity {
         ratan12MalContent.setMovementMethod(LinkMovementMethod.getInstance());
         communityContent.setMovementMethod(LinkMovementMethod.getInstance());
         acknowledgementsContent.setMovementMethod(LinkMovementMethod.getInstance());
+        translationsContent.setMovementMethod(LinkMovementMethod.getInstance());
+        notListedContent.setMovementMethod(LinkMovementMethod.getInstance());
 
         NfcHelper.disableBeam(this);
     }
