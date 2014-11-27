46a7ca592dc1aa056d1695eeee4bc85d4c7d4372
ratan12 <ratandhawtal@hotmail.com>
2014-11-27 20:05:39 +0100

    Cleanup method for the links (About)

--- Atarashii/src/net/somethingdreadful/MAL/AboutActivity.java
+++ Atarashii/src/net/somethingdreadful/MAL/AboutActivity.java
@@ -23,27 +23,21 @@ public class AboutActivity extends ActionBarActivity {
         ((Card) findViewById(R.id.community_card)).setContent(R.layout.card_about_community);
         ((Card) findViewById(R.id.translations_card)).setContent(R.layout.card_about_translations);
         ((Card) findViewById(R.id.acknowledgements_card)).setContent(R.layout.card_about_acknowledgements);
-        TextView animaMalContent = (TextView) findViewById(R.id.contributor_anima_name);
-        TextView motokochanMalContent = (TextView) findViewById(R.id.contributor_motokochan_name);
-        TextView apkawaMalContent = (TextView) findViewById(R.id.contributor_apkawa_name);
-        TextView dskoMalContent = (TextView) findViewById(R.id.contributor_dsko_name);
-        TextView ratan12MalContent = (TextView) findViewById(R.id.contributor_ratan12_name);
-        TextView acknowledgementsContent = (TextView) findViewById(R.id.acknowledgements_card_content);
-        TextView communityContent = (TextView) findViewById(R.id.community_card_content);
-        TextView translationsContent = (TextView) findViewById(R.id.translations_card_content);
-        TextView notListedContent = (TextView) findViewById(R.id.notlisted_content);
-
-        animaMalContent.setMovementMethod(LinkMovementMethod.getInstance());
-        motokochanMalContent.setMovementMethod(LinkMovementMethod.getInstance());
-        apkawaMalContent.setMovementMethod(LinkMovementMethod.getInstance());
-        dskoMalContent.setMovementMethod(LinkMovementMethod.getInstance());
-        ratan12MalContent.setMovementMethod(LinkMovementMethod.getInstance());
-        communityContent.setMovementMethod(LinkMovementMethod.getInstance());
-        acknowledgementsContent.setMovementMethod(LinkMovementMethod.getInstance());
-        translationsContent.setMovementMethod(LinkMovementMethod.getInstance());
-        notListedContent.setMovementMethod(LinkMovementMethod.getInstance());
+        createLinks((TextView) findViewById(R.id.contributor_anima_name));
+        createLinks((TextView) findViewById(R.id.contributor_motokochan_name));
+        createLinks((TextView) findViewById(R.id.contributor_apkawa_name));
+        createLinks((TextView) findViewById(R.id.contributor_dsko_name));
+        createLinks((TextView) findViewById(R.id.contributor_ratan12_name));
+        createLinks((TextView) findViewById(R.id.acknowledgements_card_content));
+        createLinks((TextView) findViewById(R.id.community_card_content));
+        createLinks((TextView) findViewById(R.id.translations_card_content));
+        createLinks((TextView) findViewById(R.id.notlisted_content));
 
         NfcHelper.disableBeam(this);
+    }
+
+    public void createLinks(TextView textView){
+        textView.setMovementMethod(LinkMovementMethod.getInstance());
     }
 
     @Override
