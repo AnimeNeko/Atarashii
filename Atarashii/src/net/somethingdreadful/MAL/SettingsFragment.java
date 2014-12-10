4381d1a062c379d3ec6a9da5e3eda708c04df6fe
ratan12 <ratandhawtal@hotmail.com>
2014-12-10 16:36:22 +0100

    Fix preference crash after the user pressed quickly back (Settings)

--- Atarashii/src/net/somethingdreadful/MAL/SettingsFragment.java
+++ Atarashii/src/net/somethingdreadful/MAL/SettingsFragment.java
@@ -8,6 +8,9 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
+import android.util.Log;
+
+import com.crashlytics.android.Crashlytics;
 
 import net.somethingdreadful.MAL.account.AccountService;
 
@@ -29,25 +32,28 @@ public class SettingsFragment extends PreferenceFragment implements SharedPrefer
 
     @Override
     public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
-        String Auth = AccountService.getAuth();
-        Bundle bundle = new Bundle();
-        int interval = Prefs.getSyncTime() * 60;
-        if (key.equals("synchronisation_time")) {
-            ContentResolver.removePeriodicSync(AccountService.getAccount(context), Auth, bundle);
-            ContentResolver.addPeriodicSync(AccountService.getAccount(context), Auth, bundle, interval);
-        } else if (key.equals("synchronisation")) {
-            if (Prefs.getSyncEnabled()) {
-                ContentResolver.setSyncAutomatically(AccountService.getAccount(context), Auth, true);
-                ContentResolver.addPeriodicSync(AccountService.getAccount(context), Auth, bundle, interval);
-            } else {
+        try {
+            String Auth = AccountService.getAuth();
+            Bundle bundle = new Bundle();
+            int interval = Prefs.getSyncTime() * 60;
+            if (key.equals("synchronisation_time")) {
                 ContentResolver.removePeriodicSync(AccountService.getAccount(context), Auth, bundle);
-                ContentResolver.setSyncAutomatically(AccountService.getAccount(context), Auth, false);
+                ContentResolver.addPeriodicSync(AccountService.getAccount(context), Auth, bundle, interval);
+            } else if (key.equals("synchronisation")) {
+                if (Prefs.getSyncEnabled()) {
+                    ContentResolver.setSyncAutomatically(AccountService.getAccount(context), Auth, true);
+                    ContentResolver.addPeriodicSync(AccountService.getAccount(context), Auth, bundle, interval);
+                } else {
+                    ContentResolver.removePeriodicSync(AccountService.getAccount(context), Auth, bundle);
+                    ContentResolver.setSyncAutomatically(AccountService.getAccount(context), Auth, false);
+                }
+            } else if (key.equals("locale")) {
+                sharedPreferences.edit().commit();
+                startActivity(new Intent(context, Home.class));
+                System.exit(0);
             }
-        } else if (key.equals("locale")) {
-            sharedPreferences.edit().commit();
-            Intent Home = new Intent(context, Home.class);
-            startActivity(Home);
-            System.exit(0);
+        }catch (Exception e) {
+            Crashlytics.log(Log.ERROR, "MALX", "SettingsFragment.onSharedPreferenceChanged(): " + e.getMessage());
         }
     }
 }
