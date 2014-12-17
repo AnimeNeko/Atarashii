d7fe40deafea09f5a072ca9a86376a776c7914ae
ratan12 <ratandhawtal@hotmail.com>
2014-12-17 12:10:39 +0100

    Add new information dialog (Dialog)

---
+++ Atarashii/src/net/somethingdreadful/MAL/dialog/InformationDialogFragment.java
@@ -0,0 +1,25 @@
+package net.somethingdreadful.MAL.dialog;
+
+import android.app.AlertDialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+public class InformationDialogFragment extends DialogFragment {
+
+    @Override
+    public AlertDialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle(getArguments().getString("title"));
+        builder.setMessage(getArguments().getString("message"));
+
+        builder.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dismiss();
+            }
+        });
+
+        return builder.create();
+    }
+}
