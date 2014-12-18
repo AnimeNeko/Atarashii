2b4c7aba6ee7bd17466ac479e50c8ab43cafcbba
ratan12 <ratandhawtal@hotmail.com>
2014-12-18 19:04:33 +0100

    Add menu to the actionbar (Forum)

--- Atarashii/src/net/somethingdreadful/MAL/ForumActivity.java
+++ Atarashii/src/net/somethingdreadful/MAL/ForumActivity.java
@@ -1,8 +1,11 @@
 package net.somethingdreadful.MAL;
 
 import android.app.FragmentManager;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ViewFlipper;
 
@@ -15,6 +18,8 @@ public class ForumActivity extends ActionBarActivity {
     ForumsPosts posts;
     FragmentManager manager;
     ViewFlipper viewFlipper;
+    TaskJob task = TaskJob.BOARD;
+    Menu menu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,17 +47,17 @@ public class ForumActivity extends ActionBarActivity {
 
     public void getTopics(int id) {
         viewFlipper.setDisplayedChild(1);
-        topics.setId(id, TaskJob.TOPICS);
+        task = topics.setId(id, TaskJob.TOPICS);
     }
 
     public void getSubBoard(int id) {
         viewFlipper.setDisplayedChild(1);
-        topics.setId(id, TaskJob.SUBBOARD);
+        task = topics.setId(id, TaskJob.SUBBOARD);
     }
 
     public void getPosts(int id) {
         viewFlipper.setDisplayedChild(2);
-        posts.setId(id);
+        task = posts.setId(id);
     }
 
     private void back() {
@@ -60,6 +65,8 @@ public class ForumActivity extends ActionBarActivity {
             viewFlipper.setDisplayedChild(viewFlipper.getDisplayedChild() - 1);
         else
             finish();
+        if (viewFlipper.getDisplayedChild() == 0)
+            task = TaskJob.BOARD;
     }
 
     @Override
@@ -68,12 +75,43 @@ public class ForumActivity extends ActionBarActivity {
     }
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_forum, menu);
+        this.menu = menu;
+        return true;
+    }
+
+    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case android.R.id.home:
                 back();
                 break;
+            case R.id.action_ViewMALPage:
+                startActivity(new Intent(Intent.ACTION_VIEW, getUri()));
+                break;
+            case R.id.action_add:
+                break;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public Uri getUri() {
+        Uri url = Uri.EMPTY;
+        switch (task) {
+            case BOARD:
+                url = Uri.parse("http://myanimelist.net/forum/");
+                break;
+            case SUBBOARD:
+                url = Uri.parse("http://myanimelist.net/forum/?subboard=" + topics.id);
+                break;
+            case TOPICS:
+                url = Uri.parse("http://myanimelist.net/forum/?board=" + topics.id);
+                break;
+            case POSTS:
+                url = Uri.parse("http://myanimelist.net/forum/?topicid=" + posts.id);
+                break;
+        }
+        return url;
     }
 }
