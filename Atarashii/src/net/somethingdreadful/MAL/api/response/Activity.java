19982533813f19dc1d814b93b80817750f08e2da
ratan12 <ratandhawtal@hotmail.com>
2015-03-01 16:28:22 +0100

    Add new activity response model (AL Profile Activity)

---
+++ Atarashii/src/net/somethingdreadful/MAL/api/response/Activity.java
@@ -0,0 +1,21 @@
+package net.somethingdreadful.MAL.api.response;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+import lombok.Getter;
+import lombok.Setter;
+
+public class Activity implements Serializable {
+    @Setter @Getter private int id;
+    @Setter @Getter @SerializedName("user_id") private int userId;
+    @Setter @Getter @SerializedName("reply_count") private int replyCount;
+    @Setter @Getter @SerializedName("created_at") private String createdAt;
+    @Setter @Getter private String status;
+    @Setter @Getter private String value;
+    @Setter @Getter private String activity_type;
+    @Setter @Getter private ArrayList<Profile> users;
+    @Setter @Getter private Anime series;
+}
