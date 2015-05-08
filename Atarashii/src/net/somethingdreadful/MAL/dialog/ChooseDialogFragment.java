aa1963b24c4c209c33491fb8bff21d49700d14d2
ratan12 <ratandhawtal@hotmail.com>
2015-05-08 17:48:40 +0200

    Replace logout dialog with universal chooseDialog

--- Atarashii/src/net/somethingdreadful/MAL/dialog/ChooseDialogFragment.java
+++ Atarashii/src/net/somethingdreadful/MAL/dialog/ChooseDialogFragment.java
@@ -5,29 +5,40 @@ import android.app.DialogFragment;
 import android.content.DialogInterface;
 import android.os.Bundle;
 
-import net.somethingdreadful.MAL.Home;
 import net.somethingdreadful.MAL.R;
 
-public class LogoutConfirmationDialogFragment extends DialogFragment {
+public class ChooseDialogFragment extends DialogFragment {
+    private onClickListener callback;
 
     @Override
     public AlertDialog onCreateDialog(Bundle savedInstanceState) {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-        builder.setTitle(R.string.dialog_title_logout);
-        builder.setMessage(R.string.dialog_message_logout);
+        builder.setTitle(getArguments().getString("title"));
+        builder.setMessage(getArguments().getString("message"));
         builder.setNegativeButton(R.string.dialog_label_cancel, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int whichButton) {
                 dismiss();
             }
         });
-        builder.setPositiveButton(R.string.dialog_label_logout, new DialogInterface.OnClickListener() {
+        builder.setPositiveButton(getArguments().getString("positive"), new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int whichButton) {
-                ((Home) getActivity()).onLogoutConfirmed();
+                callback.onPositiveButtonClicked();
                 dismiss();
             }
         });
         return builder.create();
+    }
+
+    public void setCallback(onClickListener callback) {
+        this.callback = callback;
+    }
+
+    /**
+     * The interface for callback
+     */
+    public interface onClickListener {
+        void onPositiveButtonClicked();
     }
 }
