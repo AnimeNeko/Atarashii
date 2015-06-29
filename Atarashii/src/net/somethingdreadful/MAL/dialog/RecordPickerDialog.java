b098f6235f434086e8e2260506b6bb7c1c814993
ratan12 <ratandhawtal@hotmail.com>
2015-06-29 19:15:15 +0200

    Add widget & Config activity

---
+++ Atarashii/src/net/somethingdreadful/MAL/dialog/RecordPickerDialog.java
@@ -0,0 +1,306 @@
+package net.somethingdreadful.MAL.dialog;
+
+import android.annotation.SuppressLint;
+import android.app.NotificationManager;
+import android.appwidget.AppWidgetManager;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import net.somethingdreadful.MAL.FirstTimeInit;
+import net.somethingdreadful.MAL.IGF;
+import net.somethingdreadful.MAL.NfcHelper;
+import net.somethingdreadful.MAL.PrefManager;
+import net.somethingdreadful.MAL.R;
+import net.somethingdreadful.MAL.Theme;
+import net.somethingdreadful.MAL.account.AccountService;
+import net.somethingdreadful.MAL.adapters.IGFPagerAdapter;
+import net.somethingdreadful.MAL.api.MALApi;
+import net.somethingdreadful.MAL.sql.DatabaseManager;
+import net.somethingdreadful.MAL.tasks.APIAuthenticationErrorListener;
+import net.somethingdreadful.MAL.tasks.TaskJob;
+import net.somethingdreadful.MAL.widgets.Widget1;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+
+public class RecordPickerDialog extends ActionBarActivity implements IGF.IGFCallbackListener, APIAuthenticationErrorListener, ViewPager.OnPageChangeListener {
+    IGF af;
+    IGF mf;
+    Menu menu;
+    Context context;
+    ActionBar actionBar;
+    IGFPagerAdapter mIGFPagerAdapter;
+
+    @InjectView(R.id.pager) ViewPager mViewPager;
+
+    String username;
+    boolean callbackAnimeError = false;
+    boolean callbackMangaError = false;
+    int callbackCounter = 0;
+    private int widgetID;
+    private int recordID;
+    private MALApi.ListType type;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setResult(RESULT_CANCELED);
+        context = getApplicationContext();
+        if (AccountService.getAccount() != null) {
+            actionBar = getSupportActionBar();
+            //The following is state handling code
+
+            setContentView(R.layout.activity_home);
+            // Creates the adapter to return the Animu and Mango fragments
+            mIGFPagerAdapter = new IGFPagerAdapter(getFragmentManager(), false);
+            ButterKnife.inject(this);
+
+            username = AccountService.getUsername();
+
+            // Set up the ViewPager with the sections adapter.
+            mViewPager.setAdapter(mIGFPagerAdapter);
+            mViewPager.setPageMargin(32);
+            mViewPager.setOnPageChangeListener(this);
+
+            widgetID = AppWidgetManager.INVALID_APPWIDGET_ID;
+            Intent intent = getIntent();
+            Bundle extras = intent.getExtras();
+            type = (MALApi.ListType) intent.getSerializableExtra("recordType");
+            recordID = intent.getIntExtra("recordID", 0);
+            if (extras != null) {
+                widgetID = extras.getInt(AppWidgetManager.EXTRA_APPWIDGET_ID, AppWidgetManager.INVALID_APPWIDGET_ID);
+            }
+            if (widgetID == AppWidgetManager.INVALID_APPWIDGET_ID && recordID == 0) {
+                finish();
+            }
+        } else {
+            Intent firstRunInit = new Intent(this, FirstTimeInit.class);
+            startActivity(firstRunInit);
+            finish();
+        }
+        NfcHelper.disableBeam(this);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        checkIGF();
+        switch (item.getItemId()) {
+            case R.id.listType_all:
+                getRecords(true, TaskJob.GETLIST, 0);
+                setChecked(item);
+                break;
+            case R.id.listType_inprogress:
+                getRecords(true, TaskJob.GETLIST, 1);
+                setChecked(item);
+                break;
+            case R.id.listType_completed:
+                getRecords(true, TaskJob.GETLIST, 2);
+                setChecked(item);
+                break;
+            case R.id.listType_onhold:
+                getRecords(true, TaskJob.GETLIST, 3);
+                setChecked(item);
+                break;
+            case R.id.listType_dropped:
+                getRecords(true, TaskJob.GETLIST, 4);
+                setChecked(item);
+                break;
+            case R.id.listType_planned:
+                getRecords(true, TaskJob.GETLIST, 5);
+                setChecked(item);
+                break;
+            case R.id.listType_rewatching:
+                getRecords(true, TaskJob.GETLIST, 6);
+                setChecked(item);
+                break;
+            case R.id.forceSync:
+                synctask(true);
+                break;
+            case R.id.menu_inverse:
+                if (af != null && mf != null) {
+                    if (!AccountService.isMAL() && af.taskjob == TaskJob.GETMOSTPOPULAR) {
+                        af.toggleAiringTime();
+                    } else {
+                        af.inverse();
+                        mf.inverse();
+                    }
+                }
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * On some devices the af & mf will change into null due inactivity.
+     * This is a check to prevent any crashes and set it again.
+     */
+    public void checkIGF() {
+        if (af == null || mf == null) {
+            af = (IGF) mIGFPagerAdapter.getIGF(mViewPager, 0);
+            mf = (IGF) mIGFPagerAdapter.getIGF(mViewPager, 1);
+        }
+    }
+
+    public void getRecords(boolean clear, TaskJob task, int list) {
+        checkIGF();
+        if (af != null && mf != null) {
+            af.getRecords(clear, task, list);
+            mf.getRecords(clear, task, list);
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @SuppressLint("NewApi")
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    public void synctask(boolean clear) {
+        getRecords(clear, TaskJob.FORCESYNC, af.list);
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        this.menu = menu;
+        if (af != null) {
+            //All this is handling the ticks in the switch list menu
+            switch (af.list) {
+                case 0:
+                    setChecked(menu.findItem(R.id.listType_all));
+                    break;
+                case 1:
+                    setChecked(menu.findItem(R.id.listType_inprogress));
+                    break;
+                case 2:
+                    setChecked(menu.findItem(R.id.listType_completed));
+                    break;
+                case 3:
+                    setChecked(menu.findItem(R.id.listType_onhold));
+                    break;
+                case 4:
+                    setChecked(menu.findItem(R.id.listType_dropped));
+                    break;
+                case 5:
+                    setChecked(menu.findItem(R.id.listType_planned));
+                    break;
+                case 6:
+                    setChecked(menu.findItem(R.id.listType_rewatching));
+                    break;
+            }
+            menu.findItem(R.id.action_search).setVisible(false);
+        }
+        return true;
+    }
+
+    public void setChecked(MenuItem item) {
+        item.setChecked(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_home, menu);
+        return true;
+    }
+
+    @Override
+    public void onIGFReady(IGF igf) {
+        igf.setUsername(AccountService.getUsername());
+        if (igf.listType.equals(MALApi.ListType.ANIME))
+            af = igf;
+        else
+            mf = igf;
+        // do forced sync after FirstInit
+        if (PrefManager.getForceSync()) {
+            if (af != null && mf != null) {
+                PrefManager.setForceSync(false);
+                PrefManager.commitChanges();
+                synctask(true);
+            }
+        } else {
+            if (igf.taskjob == null) {
+                igf.getRecords(true, TaskJob.GETLIST, PrefManager.getDefaultList());
+            }
+        }
+    }
+
+    @Override
+    public void onRecordsLoadingFinished(MALApi.ListType type, TaskJob job, boolean error, boolean resultEmpty, boolean cancelled) {
+        if (cancelled && !job.equals(TaskJob.FORCESYNC)) {
+            return;
+        }
+
+        callbackCounter++;
+
+        if (type.equals(MALApi.ListType.ANIME))
+            callbackAnimeError = error;
+        else
+            callbackMangaError = error;
+
+        if (callbackCounter >= 2) {
+            callbackCounter = 0;
+
+            if (job.equals(TaskJob.FORCESYNC)) {
+                NotificationManager nm = (NotificationManager) getApplicationContext().getSystemService(Context.NOTIFICATION_SERVICE);
+                nm.cancel(R.id.notification_sync);
+                if (callbackAnimeError && callbackMangaError) // the sync failed completely
+                    Theme.Snackbar(this, R.string.toast_error_SyncFailed);
+                else if (callbackAnimeError || callbackMangaError) // one list failed to sync
+                    Theme.Snackbar(this, callbackAnimeError ? R.string.toast_error_Anime_Sync : R.string.toast_error_Manga_Sync);
+            } else {
+                if (callbackAnimeError && callbackMangaError) // the sync failed completely
+                    Theme.Snackbar(this, R.string.toast_error_Records);
+                else if (callbackAnimeError || callbackMangaError) // one list failed to sync
+                    Theme.Snackbar(this, callbackAnimeError ? R.string.toast_error_Anime_Records : R.string.toast_error_Manga_Records);
+                // no else here, there is nothing to be shown when everything went well
+            }
+        }
+    }
+
+    @Override
+    public void onItemClick(int id, MALApi.ListType listType, String username) {
+        DatabaseManager db = new DatabaseManager(context);
+        if (recordID != 0)
+            db.updateWidgetRecord(recordID, type, id, listType);
+        else
+            db.addWidgetRecord(id, listType);
+
+        Intent updateWidgetIntent = new Intent(context, Widget1.class);
+        updateWidgetIntent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
+        updateWidgetIntent.putExtra("checkGhost", true);
+        context.sendBroadcast(updateWidgetIntent);
+
+        Intent resultValue = new Intent();
+        resultValue.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, widgetID);
+        setResult(RESULT_OK, resultValue);
+        finish();
+    }
+
+    @Override
+    public void onAPIAuthenticationError(MALApi.ListType type, TaskJob job) {
+        startActivity(new Intent(this, RecordPickerDialog.class).putExtra("updatePassword", true));
+        finish();
+    }
+
+    @Override
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+        if (menu != null)
+            menu.findItem(R.id.listType_rewatching).setTitle(getString(position == 0 ? R.string.listType_rewatching : R.string.listType_rereading));
+    }
+
+    @Override
+    public void onPageSelected(int position) {}
+
+    @Override
+    public void onPageScrollStateChanged(int state) {}
+}
---
+++ Atarashii/src/net/somethingdreadful/MAL/widgets/Widget1.java
@@ -0,0 +1,167 @@
+package net.somethingdreadful.MAL.widgets;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetHost;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
+import android.util.Log;
+import android.view.View;
+import android.widget.RemoteViews;
+
+import com.crashlytics.android.Crashlytics;
+import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Target;
+
+import net.somethingdreadful.MAL.DetailView;
+import net.somethingdreadful.MAL.PrefManager;
+import net.somethingdreadful.MAL.R;
+import net.somethingdreadful.MAL.account.AccountService;
+import net.somethingdreadful.MAL.api.MALApi;
+import net.somethingdreadful.MAL.api.response.AnimeManga.Anime;
+import net.somethingdreadful.MAL.api.response.AnimeManga.GenericRecord;
+import net.somethingdreadful.MAL.api.response.AnimeManga.Manga;
+import net.somethingdreadful.MAL.dialog.RecordPickerDialog;
+import net.somethingdreadful.MAL.sql.DatabaseManager;
+
+import java.util.ArrayList;
+
+import io.fabric.sdk.android.Fabric;
+
+public class Widget1 extends AppWidgetProvider {
+
+    @Override
+    public void onUpdate(final Context c, final AppWidgetManager widgetManager, final int[] ids) {
+        DatabaseManager db = new DatabaseManager(c);
+        final Picasso picasso = Picasso.with(c);
+        AccountService.create(c);
+        ArrayList<GenericRecord> dbWidgetRecords = db.getWidgetRecords();
+        final int number = dbWidgetRecords.size();
+        final RemoteViews views = new RemoteViews(c.getPackageName(), R.layout.widget1);
+
+        Fabric.with(c, new Crashlytics());
+        Crashlytics.log(Log.INFO, "MALX", "Widget1.onUpdate(): " + ids.length + " widgets and " + number + " records");
+
+        int updates = ids.length <= number ? ids.length : number;
+
+        for (int i = 0; i < updates; i++) {
+            final GenericRecord widgetRecord = dbWidgetRecords.get(i);
+            final int finalI = i;
+            widgetRecord.setId(widgetRecord.isAnime ? widgetRecord.getId() : widgetRecord.getId() * -1);
+
+            picasso.load(widgetRecord.getImageUrl())
+                    .into(new Target() {
+                        @Override
+                        public void onBitmapLoaded(Bitmap bitmap, Picasso.LoadedFrom from) {
+                            // Handle the + button clicks
+                            PrefManager.create(c);
+                            int watchValue = widgetRecord.isAnime ? ((Anime) widgetRecord).getWatchedEpisodes() :
+                                    PrefManager.getUseSecondaryAmountsEnabled() ? ((Manga) widgetRecord).getVolumesRead() : ((Manga) widgetRecord).getChaptersRead();
+                            int maxValue = widgetRecord.isAnime ? ((Anime) widgetRecord).getEpisodes() :
+                                    PrefManager.getUseSecondaryAmountsEnabled() ? ((Manga) widgetRecord).getVolumes() : ((Manga) widgetRecord).getChapters();
+                            if (watchValue == maxValue)
+                                views.setViewVisibility(R.id.popUpButton, View.GONE);
+                            else {
+                                views.setViewVisibility(R.id.popUpButton, View.VISIBLE);
+                                Intent addIntent = new Intent(c, Widget1.class).setAction(Intent.ACTION_EDIT).putExtra("id", widgetRecord.getId());
+                                views.setOnClickPendingIntent(R.id.popUpButton, PendingIntent.getBroadcast(c, widgetRecord.getId(), addIntent, PendingIntent.FLAG_UPDATE_CURRENT));
+                            }
+
+                            // Handle the cover clicks
+                            Intent viewIntent = new Intent(c, Widget1.class).setAction(Intent.ACTION_VIEW).putExtra("id", widgetRecord.getId());
+                            views.setOnClickPendingIntent(R.id.coverImage, PendingIntent.getBroadcast(c, widgetRecord.getId() * finalI * 10, viewIntent, PendingIntent.FLAG_UPDATE_CURRENT));
+
+                            Intent changeIntent = new Intent(c, Widget1.class).setAction(Intent.ACTION_PROVIDER_CHANGED).putExtra("id", widgetRecord.getId());
+                            views.setOnClickPendingIntent(R.id.changeRecord, PendingIntent.getBroadcast(c, widgetRecord.getId() * finalI * 100, changeIntent, PendingIntent.FLAG_UPDATE_CURRENT));
+
+                            views.setTextViewText(R.id.animeName, widgetRecord.getTitle());
+                            views.setTextViewText(R.id.watchedCount, Integer.toString(watchValue));
+
+                            views.setImageViewBitmap(R.id.coverImage, bitmap);
+                            widgetManager.updateAppWidget(ids[finalI], views);
+                        }
+
+                        @Override
+                        public void onBitmapFailed(Drawable errorDrawable) {
+                        }
+
+                        @Override
+                        public void onPrepareLoad(Drawable placeHolderDrawable) {
+                        }
+                    });
+        }
+    }
+
+    public static void forceRefresh(Context context) {
+        Intent updateWidgetIntent = new Intent(context, Widget1.class);
+        updateWidgetIntent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
+        context.sendBroadcast(updateWidgetIntent);
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        super.onReceive(context, intent);
+        int[] ids = AppWidgetManager.getInstance(context).getAppWidgetIds(new ComponentName(context, Widget1.class));
+        AccountService.create(context);
+        DatabaseManager db = new DatabaseManager(context);
+        Fabric.with(context, new Crashlytics());
+        int id = intent.getIntExtra("id", 0);
+
+        switch (intent.getAction()) {
+            case Intent.ACTION_EDIT:
+                if (id > 0) {
+                    Anime anime = db.getAnime(id, AccountService.getUsername());
+                    anime.setWatchedEpisodes(anime.getWatchedEpisodes() + 1);
+                    db.saveAnime(anime, false, AccountService.getUsername());
+                } else {
+                    PrefManager.create(context);
+                    Manga manga = db.getManga(id * -1, AccountService.getUsername());
+                    if (PrefManager.getUseSecondaryAmountsEnabled())
+                        manga.setVolumesRead(manga.getVolumesRead() + 1);
+                    else
+                        manga.setChaptersRead(manga.getChaptersRead() + 1);
+                    db.saveManga(manga, false, AccountService.getUsername());
+                }
+                onUpdate(context, AppWidgetManager.getInstance(context), ids);
+                break;
+            case Intent.ACTION_PROVIDER_CHANGED:
+                Intent changeRecord = new Intent(context, RecordPickerDialog.class);
+                changeRecord.putExtra("recordID", id > 0 ? id : id * -1);
+                changeRecord.putExtra("recordType", id > 0 ? MALApi.ListType.ANIME : MALApi.ListType.MANGA);
+                changeRecord.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                context.startActivity(changeRecord);
+                break;
+            case Intent.ACTION_VIEW:
+                Intent startDetails = new Intent(context, DetailView.class);
+                startDetails.putExtra("recordID", id > 0 ? id : id * -1);
+                startDetails.putExtra("recordType", id > 0 ? MALApi.ListType.ANIME : MALApi.ListType.MANGA);
+                startDetails.putExtra("username", AccountService.getUsername());
+                startDetails.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                context.startActivity(startDetails);
+                break;
+            case AppWidgetManager.ACTION_APPWIDGET_UPDATE:
+                final int number = db.getWidgetRecords().size();
+                if (intent.getBooleanExtra("checkGhost", false)) {
+                    // Remove old widget records
+                    if (number > ids.length) {
+                        for (int i = 0; i < (number - ids.length); i++)
+                            db.removeWidgetRecord();
+                        Crashlytics.log(Log.INFO, "MALX", "Widget1.onUpdate(): Removing " + (number - ids.length) + " widget records");
+                    }
+                    // Remove ghost widgets
+                    if (ids.length > number) {
+                        for (int i = 0; i < (ids.length - number); i++)
+                            (new AppWidgetHost(context, 1)).deleteAppWidgetId(ids[ids.length - 1]);
+                        Crashlytics.log(Log.INFO, "MALX", "Widget1.onUpdate(): Removing " + (ids.length - number) + " ghost widgets");
+                        ids = AppWidgetManager.getInstance(context).getAppWidgetIds(new ComponentName(context, Widget1.class));
+                    }
+                }
+                onUpdate(context, AppWidgetManager.getInstance(context), ids);
+                break;
+        }
+    }
+}
