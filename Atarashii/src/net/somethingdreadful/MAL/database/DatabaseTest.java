dd692eed69762f320d1e7954febfab28a8fbb171
ratan12 <ratandhawtal@hotmail.com>
2015-10-30 19:30:21 +0100

    Add new Database files (Manager and helpers)

---
+++ Atarashii/src/net/somethingdreadful/MAL/database/DatabaseTest.java
@@ -0,0 +1,179 @@
+package net.somethingdreadful.MAL.database;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import com.crashlytics.android.Crashlytics;
+
+public class DatabaseTest extends SQLiteOpenHelper {
+    private static DatabaseTest instance;
+    private Context context;
+
+    public static final String NAME = "MAL.db";
+    public static final int VERSION = 13;
+
+    public static final String TABLE_ANIME = "anime";
+    public static final String TABLE_MANGA = "manga";
+    public static final String TABLE_PROFILE = "profile";
+    public static final String TABLE_FRIENDLIST = "friendlist";
+    public static final String TABLE_PRODUCER = "producer";
+    public static final String TABLE_ANIME_PRODUCER = "anime_producer";
+    public static final String TABLE_ANIME_OTHER_TITLES = "animeothertitles";
+    public static final String TABLE_MANGA_OTHER_TITLES = "mangaothertitles";
+    public static final String TABLE_ACTIVITIES = "activities";
+
+    public static final String TABLE_ANIME_ANIME_RELATIONS = "rel_anime_anime";
+    public static final String TABLE_ANIME_MANGA_RELATIONS = "rel_anime_manga";
+    public static final String TABLE_MANGA_MANGA_RELATIONS = "rel_manga_manga";
+    public static final String TABLE_MANGA_ANIME_RELATIONS = "rel_manga_anime";
+
+    public static final String RELATION_TYPE_ALTERNATIVE = "0";
+    public static final String RELATION_TYPE_CHARACTER = "1";
+    public static final String RELATION_TYPE_SIDE_STORY = "2";
+    public static final String RELATION_TYPE_SPINOFF = "3";
+    public static final String RELATION_TYPE_SUMMARY = "4";
+    public static final String RELATION_TYPE_ADAPTATION = "5";
+    public static final String RELATION_TYPE_RELATED = "6";
+    public static final String RELATION_TYPE_PREQUEL = "7";
+    public static final String RELATION_TYPE_SEQUEL = "8";
+    public static final String RELATION_TYPE_PARENT_STORY = "9";
+    public static final String RELATION_TYPE_OTHER = "10";
+
+    public static final String COLUMN_ID = "_id";
+
+    private static final String CREATE_PRODUCER_TABLE = "CREATE TABLE "
+            + TABLE_PRODUCER + "("
+            + COLUMN_ID + " integer primary key autoincrement, "
+            + "title varchar UNIQUE"
+            + ");";
+
+    private static final String CREATE_ANIME_PRODUCER_TABLE = "CREATE TABLE "
+            + TABLE_ANIME_PRODUCER + "("
+            + "anime_id integer NOT NULL REFERENCES " + TABLE_ANIME + "(" + COLUMN_ID + ") ON DELETE CASCADE, "
+            + "producer_id integer NOT NULL REFERENCES " + TABLE_PRODUCER + "(" + COLUMN_ID + ") ON DELETE CASCADE, "
+            + "PRIMARY KEY(anime_id, producer_id)"
+            + ");";
+
+ /*
+  * Anime-/Manga-relation tables
+  *
+  * Structure for these tables is
+  * - anime id
+  * - related id
+  * - relation type (side story, summary, alternative version etc.), see RELATION_TYPE-constants
+  * below
+  */
+
+ /* relation types, as these constants are only used for database queries they can be stored as strings
+  * to avoid conversion in every query
+  */
+
+    public static final String TABLE_GENRES = "genres";
+    private static final String CREATE_GENRES_TABLE = "CREATE TABLE "
+            + TABLE_GENRES + "("
+            + COLUMN_ID + " integer primary key autoincrement, "
+            + "title varchar NOT NULL "
+            + ");";
+    public static final String TABLE_ANIME_GENRES = "anime_genres";
+    private static final String CREATE_ANIME_GENRES_TABLE = "CREATE TABLE "
+            + TABLE_ANIME_GENRES + "("
+            + "anime_id integer NOT NULL REFERENCES " + TABLE_ANIME + "(" + COLUMN_ID + ") ON DELETE CASCADE, "
+            + "genre_id integer NOT NULL REFERENCES " + TABLE_GENRES + "(" + COLUMN_ID + ") ON DELETE CASCADE, "
+            + "PRIMARY KEY(anime_id, genre_id)"
+            + ");";
+    public static final String TABLE_MANGA_GENRES = "manga_genres";
+    private static final String CREATE_MANGA_GENRES_TABLE = "CREATE TABLE "
+            + TABLE_MANGA_GENRES + "("
+            + "manga_id integer NOT NULL REFERENCES " + TABLE_MANGA + "(" + COLUMN_ID + ") ON DELETE CASCADE, "
+            + "genre_id integer NOT NULL REFERENCES " + TABLE_GENRES + "(" + COLUMN_ID + ") ON DELETE CASCADE, "
+            + "PRIMARY KEY(manga_id, genre_id)"
+            + ");";
+
+    public static final String TABLE_TAGS = "tags";
+    private static final String CREATE_TAGS_TABLE = "CREATE TABLE "
+            + TABLE_TAGS + "("
+            + COLUMN_ID + " integer primary key autoincrement, "
+            + "title varchar NOT NULL "
+            + ");";
+    public static final String TABLE_ANIME_TAGS = "anime_tags";
+    public static final String TABLE_ANIME_PERSONALTAGS = "anime_personaltags";
+    public static final String TABLE_MANGA_TAGS = "manga_tags";
+    public static final String TABLE_MANGA_PERSONALTAGS = "manga_personaltags";
+    /* title types, working the same way as the relation types
+     */
+    public static final String TITLE_TYPE_JAPANESE = "0";
+    public static final String TITLE_TYPE_ENGLISH = "1";
+    public static final String TITLE_TYPE_SYNONYM = "2";
+    ;
+
+    private static final String CREATE_ACTIVITIES_TABLE = "CREATE TABLE "
+            + TABLE_ACTIVITIES + "("
+            + COLUMN_ID + " integer primary key, "
+            + "type varchar NOT NULL, "
+            + "created varchar NOT NULL, "
+            + "reply_count integer NOT NULL, "
+            + "series_anime integer, "
+            + "series_manga integer, "
+            + "status varchar, "
+            + "value varchar"
+            + ");";
+
+    public DatabaseTest(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    public DatabaseTest(Context context) {
+        super(context, NAME, null, VERSION);
+    }
+
+    public static void deleteDatabase(Context context) {
+        instance = null;
+        context.deleteDatabase(NAME);
+    }
+
+    @Override
+    public String getDatabaseName() {
+        return NAME;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        Table.create(db).createRecord(TABLE_ANIME);
+        Table.create(db).createRecord(TABLE_MANGA);
+        Table.create(db).createProfile();
+        Table.create(db).createFriendlist();
+        Table.create(db).createRelation(TABLE_ANIME_ANIME_RELATIONS, TABLE_ANIME, TABLE_ANIME);
+        Table.create(db).createRelation(TABLE_ANIME_MANGA_RELATIONS, TABLE_ANIME, TABLE_MANGA);
+        Table.create(db).createRelation(TABLE_MANGA_MANGA_RELATIONS, TABLE_MANGA, TABLE_MANGA);
+        Table.create(db).createRelation(TABLE_MANGA_ANIME_RELATIONS, TABLE_MANGA, TABLE_ANIME);
+        Table.create(db).createTags(TABLE_ANIME_TAGS, TABLE_ANIME, TABLE_TAGS);
+        Table.create(db).createTags(TABLE_MANGA_TAGS, TABLE_MANGA, TABLE_TAGS);
+        Table.create(db).createTags(TABLE_ANIME_PERSONALTAGS, TABLE_ANIME, TABLE_TAGS);
+        Table.create(db).createTags(TABLE_MANGA_PERSONALTAGS, TABLE_MANGA, TABLE_TAGS);
+        db.execSQL(CREATE_GENRES_TABLE);
+        db.execSQL(CREATE_ANIME_GENRES_TABLE);
+        db.execSQL(CREATE_MANGA_GENRES_TABLE);
+        db.execSQL(CREATE_TAGS_TABLE);
+        Table.create(db).createOtherTitles(TABLE_ANIME_OTHER_TITLES, TABLE_ANIME);
+        Table.create(db).createOtherTitles(TABLE_MANGA_OTHER_TITLES, TABLE_MANGA);
+        db.execSQL(CREATE_PRODUCER_TABLE);
+        db.execSQL(CREATE_ANIME_PRODUCER_TABLE);
+        db.execSQL(CREATE_ACTIVITIES_TABLE);
+    }
+
+    @Override
+    public void onOpen(SQLiteDatabase db) {
+        super.onOpen(db);
+        if (!db.isReadOnly()) {
+            // Enable foreign key constraints
+            db.execSQL("PRAGMA foreign_keys=ON;");
+        }
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        Crashlytics.log(Log.VERBOSE, "MALX", "MALSQLHelper.OnUpgrade: Upgrading database from version " + oldVersion + " to " + newVersion);
+    }
+}
---
+++ Atarashii/src/net/somethingdreadful/MAL/database/Table.java
@@ -0,0 +1,196 @@
+package net.somethingdreadful.MAL.database;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
+
+import com.crashlytics.android.Crashlytics;
+
+public class Table {
+    String queryString = "";
+    private static SQLiteDatabase db;
+
+    public static Table create(SQLiteDatabase db) {
+        Table.db = db;
+        return new Table();
+    }
+
+    public void createOtherTitles(String table, String ListTypeTable) {
+        queryString += "CREATE TABLE "
+                + table + "("
+                + getTagsColumn(table) + " integer NOT NULL REFERENCES " + ListTypeTable + "(" + DatabaseTest.COLUMN_ID + ") ON DELETE CASCADE, "
+                + "titleType integer NOT NULL, "
+                + "title varchar NOT NULL, "
+                + "PRIMARY KEY(" + getTagsColumn(table) + ", titleType, title)"
+                + ");";
+        run();
+    }
+
+    public void createRecord(String table) {
+        queryString += "create table "
+                + table + "("
+                + DatabaseTest.COLUMN_ID + " integer primary key, "
+                + "title varchar, "
+                + "type varchar, "
+                + "imageUrl varchar, "
+                + "synopsis varchar, "
+                + "status varchar, "
+                + "startDate varchar, "
+                + "endDate varchar, "
+                + "score integer, "
+                + "priority integer, "
+                + "classification varchar, "
+                + "averageScore varchar, "
+                + "averageScoreCount varchar, "
+                + "popularity integer, "
+                + "rank integer, "
+                + "notes varchar, "
+                + "favoritedCount integer, "
+                + "dirty varchar, "
+                + "createFlag integer, "
+                + "deleteFlag integer, "
+                + "widget integer, ";
+
+
+        if (table.equals(DatabaseTest.TABLE_ANIME))
+            queryString += "duration integer, "
+                    + "episodes integer, "
+                    + "youtubeId varchar, "
+                    //+ "listStats , " TODO: investigate what this really is
+                    + "airingTime varchar, "
+                    + "nextEpisode integer, "
+                    + "watchedStatus varchar, "
+                    + "watchedEpisodes integer, "
+                    + "watchingStart varchar, "
+                    + "watchingEnd varchar, "
+                    + "fansubGroup varchar, "
+                    + "storage integer, "
+                    + "storageValue float, "
+                    + "epsDownloaded integer, "
+                    + "rewatching integer, "
+                    + "rewatchCount integer, "
+                    + "rewatchValue integer "
+                    + ");";
+        else
+            queryString += "chapters integer, "
+                    + "volumes integer, "
+                    + "readStatus varchar, "
+                    + "chaptersRead integer, "
+                    + "volumesRead integer, "
+                    + "readingStart varchar, "
+                    + "readingEnd varchar, "
+                    + "chapDownloaded integer, "
+                    + "rereading integer, "
+                    + "rereadCount integer, "
+                    + "rereadValue integer "
+                    + ");";
+        run();
+    }
+
+    public void createFriendlist() {
+        queryString += "create table "
+                + DatabaseTest.TABLE_FRIENDLIST + "("
+                + "username varchar, "
+                + "imageUrl varchar, "
+                + "lastOnline varchar "
+                + ");";
+        run();
+    }
+
+    /**
+     * Create the profile table.
+     */
+    public void createProfile(){
+        queryString += "create table "
+                + DatabaseTest.TABLE_PROFILE + "("
+                + "username varchar UNIQUE, "
+                + "imageUrl varchar, "
+                + "imageUrlBanner varchar, "
+                + "notifications integer, "
+                + "lastOnline varchar, "
+                + "status varchar, "
+                + "gender varchar, "
+                + "birthday varchar, "
+                + "location varchar, "
+                + "website varchar, "
+                + "joinDate varchar, "
+                + "accessRank varchar, "
+                + "animeListViews integer, "
+                + "mangaListViews integer, "
+                + "forumPosts integer, "
+                + "comments integer, "
+
+                + "AnimetimeDays double, "
+                + "Animewatching integer, "
+                + "Animecompleted integer, "
+                + "AnimeonHold integer, "
+                + "Animedropped integer, "
+                + "AnimeplanToWatch integer, "
+                + "AnimetotalEntries integer, "
+
+                + "MangatimeDays double, "
+                + "Mangareading integer, "
+                + "Mangacompleted integer, "
+                + "MangaonHold integer, "
+                + "Mangadropped integer, "
+                + "MangaplanToRead integer, "
+                + "MangatotalEntries integer "
+                + ");";
+        run();
+    }
+
+    /**
+     * Create tags table.
+     *
+     * @param table The table name
+     * @param refTable1 The table that should get referenced
+     * @param refTable2 The table that will be referenced with
+     */
+    public void createTags(String table, String refTable1, String refTable2) {
+        queryString += "CREATE TABLE " + table + "("
+                + getTagsColumn(table) + " integer NOT NULL REFERENCES " + refTable1 + "(" + DatabaseTest.COLUMN_ID + ") ON DELETE CASCADE, "
+                + "tag_id integer NOT NULL REFERENCES " + refTable2 + "(" + DatabaseTest.COLUMN_ID + ") ON DELETE CASCADE, "
+                + "PRIMARY KEY(" + getTagsColumn(table) + ", tag_id)"
+                + ");";
+        run();
+    }
+
+    /**
+     * Get the id column name
+     *
+     * @param table The table name
+     * @return String The id column name
+     */
+    private String getTagsColumn(String table) {
+        return table.contains("anime") ? "anime_id" : "manga_id";
+    }
+
+    /**
+     * Create relation table.
+     *
+     * @param table The table name
+     * @param refTable1 The table that should get referenced
+     * @param refTable2 The table that will be referenced with
+     */
+    public void createRelation(String table, String refTable1, String refTable2) {
+        queryString += "CREATE TABLE " + table + "("
+                + DatabaseTest.COLUMN_ID + " integer NOT NULL REFERENCES " + refTable1 + "(" + DatabaseTest.COLUMN_ID + ") ON DELETE CASCADE, "
+                + "relationId integer NOT NULL REFERENCES " + refTable2 + "(" + DatabaseTest.COLUMN_ID + ") ON DELETE CASCADE, "
+                + "relationType integer NOT NULL, "
+                + "PRIMARY KEY(" + DatabaseTest.COLUMN_ID + ", relationId)"
+                + ");";
+        run();
+    }
+
+    public void run() {
+        try {
+            db.execSQL(queryString);
+        } catch (Exception e){
+            Crashlytics.log(Log.INFO, "MALX", "Table.run(" + toString() + "): " + e.getMessage());
+        }
+    }
+
+    @Override
+    public String toString() {
+        return queryString;
+    }
+}
