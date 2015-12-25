ab93b2e86c4c9e889e114dc088481f332a44db29
ratan12 <ratandhawtal@hotmail.com>
2015-12-25 14:24:13 +0100

    Add new forum files

---
+++ Atarashii/src/net/somethingdreadful/MAL/forum/ForumInterface.java
@@ -0,0 +1,40 @@
+package net.somethingdreadful.MAL.forum;
+
+import android.webkit.JavascriptInterface;
+
+import net.somethingdreadful.MAL.ForumActivity;
+import net.somethingdreadful.MAL.tasks.ForumJob;
+
+public class ForumInterface {
+    ForumActivity forum;
+
+    public ForumInterface(ForumActivity forum) {
+        this.forum = forum;
+    }
+
+    /**
+     * Get the topics from a certain category.
+     */
+    @JavascriptInterface
+    public void tileClick(final String id) {
+        forum.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                forum.getRecords(ForumJob.CATEGORY, Integer.parseInt(id));
+            }
+        });
+    }
+
+    /**
+     * Get the posts from a certain topic.
+     */
+    @JavascriptInterface
+    public void topicClick(final String id) {
+        forum.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                forum.getRecords(ForumJob.TOPIC, Integer.parseInt(id));
+            }
+        });
+    }
+}
