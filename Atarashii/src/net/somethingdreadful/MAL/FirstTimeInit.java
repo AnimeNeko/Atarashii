5e25ecd15f48bb39fcccdf27b3314500cd8c9f09
ratan12 <ratandhawtal@hotmail.com>
2015-12-25 14:24:51 +0100

    Fix startup crashes due a null context

--- Atarashii/src/net/somethingdreadful/MAL/FirstTimeInit.java
+++ Atarashii/src/net/somethingdreadful/MAL/FirstTimeInit.java
@@ -85,7 +85,6 @@ public class FirstTimeInit extends AppCompatActivity implements AuthenticationCh
 
         PrefManager.deleteAccount();
         NfcHelper.disableBeam(this);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
     private void tryConnection() {
