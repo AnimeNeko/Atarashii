e6ac4289f08b5e6bdd5c1f5b3f4ea0b0a47ef4ec
ratan12 <ratandhawtal@hotmail.com>
2016-06-03 13:48:31 +0200

    Replace FragmentHolder with Fragments class

---
+++ Atarashii/src/net/somethingdreadful/MAL/adapters/Fragments.java
@@ -0,0 +1,65 @@
+package net.somethingdreadful.MAL.adapters;
+
+
+import android.app.Activity;
+import android.app.Fragment;
+
+import java.util.ArrayList;
+
+import lombok.Getter;
+import lombok.Setter;
+
+public class Fragments {
+    private ArrayList<FragmentHolder> fragments = new ArrayList<>();
+    Activity activity;
+
+    Fragments(Activity activity) {
+        this.activity = activity;
+    }
+
+    /**
+     * Add fragments to the holder.
+     *
+     * @param fragment The fragment which should be added
+     * @param name The fragment name res ID shown in the tabs
+     */
+    public void add(Fragment fragment, int name) {
+        add(fragment, activity.getString(name));
+    }
+
+    /**
+     * Add fragments to the holder.
+     *
+     * @param fragment The fragment which should be added
+     * @param name The fragment String name shown in the tabs
+     */
+    public void add(Fragment fragment, String name) {
+        fragments.add(new FragmentHolder(fragment, name));
+    }
+
+    public int getSize() {
+        return fragments.size();
+    }
+
+    public Fragment getFragment(int position) {
+        return fragments.get(position).getFragment();
+    }
+
+    public String getName(int position) {
+        return fragments.get(position).getName();
+    }
+
+    public class FragmentHolder {
+        @Getter
+        @Setter
+        private String name;
+        @Getter
+        @Setter
+        private Fragment fragment;
+
+        public FragmentHolder(Fragment fragment, String name) {
+            this.fragment = fragment;
+            this.name = name;
+        }
+    }
+}
--- Atarashii/src/net/somethingdreadful/MAL/adapters/ProfilePagerAdapter.java
+++ Atarashii/src/net/somethingdreadful/MAL/adapters/ProfilePagerAdapter.java
@@ -15,70 +15,45 @@ import net.somethingdreadful.MAL.profile.ProfileDetailsMAL;
 import net.somethingdreadful.MAL.profile.ProfileFriends;
 import net.somethingdreadful.MAL.profile.ProfileHistory;
 
-import java.util.ArrayList;
-
-import lombok.Getter;
-import lombok.Setter;
-
 public class ProfilePagerAdapter extends FragmentPagerAdapter {
-    private final ProfileActivity activity;
-    private ArrayList<FragmentHolder> fragments = new ArrayList<>();
+    Fragments fragments ;
 
     public ProfilePagerAdapter(FragmentManager fm, ProfileActivity activity) {
         super(fm);
-        this.activity = activity;
+        fragments = new Fragments(activity);
 
         if (AccountService.isMAL()) {
-            fragments.add(new FragmentHolder(new ProfileDetailsMAL(), R.string.tab_name_details));
-            fragments.add(new FragmentHolder(new ProfileFriends(), R.string.tab_name_friends));
+            fragments.add(new ProfileDetailsMAL(), R.string.tab_name_details);
+            fragments.add(new ProfileFriends(), R.string.tab_name_friends);
             if (APIHelper.isNetworkAvailable(activity)) {
-                fragments.add(new FragmentHolder(new ProfileHistory(), R.string.tab_name_history));
-                fragments.add(new FragmentHolder(new IGF().setFriendList(MALApi.ListType.ANIME), String.valueOf(MALApi.ListType.ANIME)));
-                fragments.add(new FragmentHolder(new IGF().setFriendList(MALApi.ListType.MANGA), String.valueOf(MALApi.ListType.MANGA)));
+                fragments.add(new ProfileHistory(), R.string.tab_name_history);
+                fragments.add(new IGF().setFriendList(MALApi.ListType.ANIME), String.valueOf(MALApi.ListType.ANIME));
+                fragments.add(new IGF().setFriendList(MALApi.ListType.MANGA), String.valueOf(MALApi.ListType.MANGA));
             }
         } else {
-            fragments.add(new FragmentHolder(new ProfileDetailsAL(), R.string.tab_name_details));
-            fragments.add(new FragmentHolder(new ProfileFriends().setId(0), R.string.tab_name_following));
+            fragments.add(new ProfileDetailsAL(), R.string.tab_name_details);
+            fragments.add(new ProfileFriends().setId(0), R.string.tab_name_following);
             if (APIHelper.isNetworkAvailable(activity)) {
-                fragments.add(new FragmentHolder(new ProfileFriends().setId(1), R.string.tab_name_followers));
-                fragments.add(new FragmentHolder(new ProfileHistory(), R.string.layout_card_title_activity));
-                fragments.add(new FragmentHolder(new IGF().setFriendList(MALApi.ListType.ANIME), String.valueOf(MALApi.ListType.ANIME)));
-                fragments.add(new FragmentHolder(new IGF().setFriendList(MALApi.ListType.MANGA), String.valueOf(MALApi.ListType.MANGA)));
+                fragments.add(new ProfileFriends().setId(1), R.string.tab_name_followers);
+                fragments.add(new ProfileHistory(), R.string.layout_card_title_activity);
+                fragments.add(new IGF().setFriendList(MALApi.ListType.ANIME), String.valueOf(MALApi.ListType.ANIME));
+                fragments.add(new IGF().setFriendList(MALApi.ListType.MANGA), String.valueOf(MALApi.ListType.MANGA));
             }
         }
     }
 
     @Override
     public Fragment getItem(int position) {
-        return fragments.get(position).getFragment();
+        return fragments.getFragment(position);
     }
 
     @Override
     public int getCount() {
-        return fragments.size();
+        return fragments.getSize();
     }
 
     @Override
     public String getPageTitle(int position) {
-        return fragments.get(position).getName();
-    }
-
-    public class FragmentHolder {
-        @Getter
-        @Setter
-        private String name;
-        @Getter
-        @Setter
-        private Fragment fragment;
-
-        public FragmentHolder(Fragment fragment, int name) {
-            this.fragment = fragment;
-            this.name = activity.getString(name);
-        }
-
-        public FragmentHolder(Fragment fragment, String name) {
-            this.fragment = fragment;
-            this.name = name;
-        }
+        return fragments.getName(position);
     }
 }
